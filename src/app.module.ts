ae6d14a30ac821d8ce6060bed658d338981b9dee
davwys <david.wyss@hotmail.ch>
2021-10-01 14:53:01 +0200

    Add some test controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DogsController } from './dogs/dogs.controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, DogsController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
