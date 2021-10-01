b67c59be88d21c9013b36a6cf9261d23f87b2b20
davwys <david.wyss@hotmail.ch>
2021-10-01 15:46:31 +0200

    Add some tests

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    @Get()
+    findAll(): string {
+        return 'This action returns all cats';
+    }
+}
