0ffa52f38a38471b541f615198432d7e29eb55b6
thommann <thomasrolf.mannhart@uzh.ch>
2021-10-04 15:32:56 +0200

    docker without fastify

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-      AppModule,
-      new FastifyAdapter({ logger: true })
-  );
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
