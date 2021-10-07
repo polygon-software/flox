f0ae0dfdf2043d6d25ab7a5aa4530707d0a73a06
davwys <david.wyss@hotmail.ch>
2021-10-07 13:36:59 +0200

    Remove test controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DogsController } from './dogs/dogs.controller';
-import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, DogsController, CatsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
