0c30cde764fc82a69a62318b1b07bf39f408d4f0
davwys <david.wyss@hotmail.ch>
2021-10-07 14:39:33 +0200

    Basic setup, add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true, // TODO config stuff goes here
+      debug: true,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'), // Auto-generates typescript definitions with AST changes
+        outputAs: 'interface', // Alternative: 'class
+      },
+      //disableHealthCheck: true //set true if using multiple GraphQL endpoints in a single application with fastify
+    }),
+    UserModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
