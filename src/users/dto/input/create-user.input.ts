6ee06c8dcd879267e8a18efcb4ff4fd9aa7a1d6e
davwys <david.wyss@hotmail.ch>
2021-10-08 09:26:40 +0200

    Add some functional test CRUDs for local testing

---
+++ src/users/dto/input/create-user.input.ts
@@ -0,0 +1,13 @@
+import {Field, InputType} from "@nestjs/graphql";
+import {IsNotEmpty, IsNumber} from "class-validator";
+
+@InputType()
+export class CreateUserInput {
+    @Field()
+    @IsNotEmpty()
+    name: string;
+
+    @Field()
+    @IsNotEmpty()
+    age: number;
+}
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,34 +1,38 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { User } from "../graphql"
+import {GetUserArgs} from "./dto/args/get-user.args";
+import {GetUsersArgs} from "./dto/args/get-users.args";
+import {DeleteUserInput} from "./dto/input/delete-user.input";
 
 @Resolver('User')
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => [User], {name: 'users', nullable: 'items'}) // only items may be null
+  getUsers(@Args() getUsersArgs: GetUsersArgs): User[] {
+    return this.usersService.getUsers(getUsersArgs);
+  }
+
+  @Query(() => User, {name: 'user', nullable: true, })
+  getUser(@Args() getUserArgs: GetUserArgs): User {
+    return this.usersService.getUser(getUserArgs);
+  }
 
   @Mutation('createUser')
   create(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.usersService.create(createUserInput);
   }
 
-  @Query('users')
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Query('user')
-  findOne(@Args('id') id: number) {
-    return this.usersService.findOne(id);
-  }
-
   @Mutation('updateUser')
   update(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
+    return this.usersService.update(updateUserInput);
   }
 
   @Mutation('removeUser')
-  remove(@Args('id') id: number) {
-    return this.usersService.remove(id);
+  remove(@Args('deleteUserInput') deleteUserInput: DeleteUserInput) {
+    return this.usersService.remove(deleteUserInput);
   }
 }
