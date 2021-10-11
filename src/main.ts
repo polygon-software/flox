0c21e637b4a982e5f64f04e8e64d76711ce9eea4
davwys <david.wyss@hotmail.ch>
2021-10-11 12:24:51 +0200

    Add comment

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,10 @@ async function bootstrap() {
   // Add GraphQL Voyager as middleware (intended for express, but seems to work on fastify as well)
   app.use('/schema', voyagerMiddleware({ endpointUrl: '/graphql' }));
 
+  // IMPORTANT: make sure to copy voyager.worker.js from node_modules/graphql-voyager/dist
+  // to the same folder as your main bundle or use workerURI property to specify other path.
+  // Not sure if this is actually relevant, TODO test
+
   await app.listen(3000);
 }
 bootstrap();
