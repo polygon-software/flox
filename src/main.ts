1dd6472d0534e45bdc00ec779da0903b91508f66
davwys <david.wyss@hotmail.ch>
2021-10-11 11:01:23 +0200

    Add GraphQL Voyager middleware

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-      AppModule,
-      new FastifyAdapter()
+    AppModule,
+    new FastifyAdapter(),
   );
+
+  // Add GraphQL Voyager as middleware (intended for express, but seems to work on fastify as well)
+  app.use('/schema', voyagerMiddleware({ endpointUrl: '/graphql' }));
+
   await app.listen(3000);
 }
 bootstrap();
