bdf1ded5722b89ec196c7ccb7cc6e044fa9892d1
thommann <thomasrolf.mannhart@uzh.ch>
2021-10-12 15:27:56 +0200

    .env

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -18,7 +19,7 @@ async function bootstrap() {
   // IMPORTANT: make sure to copy voyager.worker.js from node_modules/graphql-voyager/dist
   // to the same folder as your main bundle or use workerURI property to specify other path.
   // Not sure if this is actually relevant, TODO test
-
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+  await app.listen(configService.get('server.port'));
 }
 bootstrap();
