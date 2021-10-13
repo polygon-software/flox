3980f6d8c33ed11872c8303c513bec04424d46a0
davwys <david.wyss@hotmail.ch>
2021-10-13 07:58:57 +0200

    Default page

--- frontend/my-app/src/components/HelloWorld.vue
+++ frontend/my-app/src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="../assets/logo.svg" style="width: 200px; height: 200px">
+      <img
+          alt="Polygon Software"
+          src="https://media-exp1.licdn.com/dms/image/C4D0BAQEI1LFXsM4DVA/company-logo_200_200/0/1593964710523?e=2159024400&v=beta&t=k1qIEpVNRq-GBvW1fZt2SKvcuq59WL8J0IuLW0qMSG4"
+          style="width: 200px; height: 200px">
+      <h4>Hello World! Test Application is running.</h4>
   </q-page>
 </template>
 
