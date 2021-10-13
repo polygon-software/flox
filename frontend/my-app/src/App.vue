6d00914bffff7f03c04ba4adef1434e1867ce85d
davwys <david.wyss@hotmail.ch>
2021-10-13 09:21:01 +0200

    Add test table with GraphQL connection

--- frontend/my-app/src/App.vue
+++ frontend/my-app/src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
-      <HelloWorld />
+      <q-page class="flex flex-center column">
+        <div class="row">
+          <img
+              alt="Polygon Software"
+              src="https://media-exp1.licdn.com/dms/image/C4D0BAQEI1LFXsM4DVA/company-logo_200_200/0/1593964710523?e=2159024400&v=beta&t=k1qIEpVNRq-GBvW1fZt2SKvcuq59WL8J0IuLW0qMSG4"
+              style="width: 200px; height: 200px">
+          <h4>Hello World! Test Application is running.</h4>
+        </div>
+        <!-- Test table for visualizing data fetched via GraphQL/Apollo -->
+        <div class="row" style="margin-top: 100px">
+          <TestTable/>
+        </div>
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestTable from './components/TestTable.vue'
 
 export default {
   name: 'LayoutDefault',
 
   components: {
-    HelloWorld
+    TestTable
   },
 }
 </script>
