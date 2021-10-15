72add9006c54e1ae9f8e9474b5f02cc609baaf8a
davwys <david.wyss@hotmail.ch>
2021-10-15 10:44:18 +0200

    Add data definitions, helpers setup

--- frontend/my-app/src/data/data-helpers.ts
+++ frontend/my-app/src/data/data-helpers.ts
@@ -1,31 +1,50 @@
-import {useQuery} from "@vue/apollo-composable";
-import {ALL_USERS} from "@/data/QUERIES";
+import {useMutation, useQuery} from "@vue/apollo-composable";
+import {QUERIES} from "@/data/QUERIES";
 import {DocumentParameter} from "@vue/apollo-composable/dist/useQuery";
+import {QueryObject, MutationObject, MutationTypes} from "@/data/DATA-DEFINITIONS";
+import {DELETE_USER} from "@/data/MUTATIONS";
 
 /**
  * This file contains a collection of helper functions for querying and mutating data using GraphQL/Apollo.
  */
 
-// Interface definition as used in constant files (e.g. QUERIES.ts, MUTATIONS.ts)
-interface QueryObject {
-    query: DocumentParameter<any>, // Actual GraphQL query
-    tables: string[],              // Affected Tables
-}
-
-/***
- *
+/**
+ * Executes a given GraphQL Query object
  * @param {QueryObject} queryObject: A query object, containing the 'query' itself and a list of affected tables
  */
 function executeQuery(queryObject: QueryObject){
     const query =  queryObject.query;
-    const tables =  queryObject.tables;
 
-    // Execute query itself
-    useQuery(query)
+    // Execute query
+    useQuery(query);
 }
 
-function executeMutation(){
+/**
+ * Executes a given GraphQL Mutation object, considering cache effects
+ * @param {MutationObject} mutationObject
+ */
+function executeMutation(mutationObject: MutationObject){
+    const mutation =  mutationObject.mutation;
+    const tables =  mutationObject.tables;
+    const type =  mutationObject.type;
 
+    // Find affected queries based on tables
+    QUERIES.forEach((QUERY) => {
+
+    })
+
+    // Execute mutation and handle cache
+    useMutation(mutation, () => ({
+        update: (cache, { data: { remove } }) => {
+            const data = cache.readQuery({ query: ALL_USERS })
+            // Remove on cache
+            cache.writeQuery({ query: ALL_USERS, data: {
+                    ...data,
+                    allUsers: data.allUsers.filter(user => user.id !== remove.id)
+                }
+            })
+        },
+    }))
 }
 
 
