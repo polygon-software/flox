644a72bf1239a023e76b44c696c4beea95489dc9
Johann Schwabe <Johann.schwabe@uzh.ch>
2021-10-18 14:39:29 +0200

    wörks as intended

--- frontend/my-app/src/components/loginbutton.vue
+++ frontend/my-app/src/components/loginbutton.vue
@@ -1,8 +1,9 @@
 <template>
   <q-btn
-      label="SignUp"
-      @click="confirm_modal = true"
-  />
+    label="SignUp"
+    @click="confirm_modal = true"
+    color="primary"
+  ></q-btn>
   <q-dialog v-model="confirm_modal">
     <q-card>
       <q-card-section>
@@ -10,21 +11,21 @@
         <q-input v-model="password"/>
       </q-card-section>
       <q-card-actions align="right">
-        <q-btn flat label="Register" v-close-popup @click="signUp"/>
+        <q-btn flat label="Register" v-close-popup @click="()=>{user = signUp(email, password)}"/>
+      </q-card-actions>
+      <q-card-actions align="right">
+        <q-btn flat label="login" v-close-popup @click="()=>{user = login(email, password)}"/>
       </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
 
 <script setup lang="ts">
-var AmazonCognitoIdentity = require('amazon-cognito-identity-js')
+import {login, signUp} from "@/plugins/auth.ts";
+
 import {ref} from "vue";
 
-const poolData = {
-  UserPoolId: "eu-central-1_DGPNZZeuX",
-  ClientId: "48k1t49g64el9v2m0ojv5dh0p7"
-};
-const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData)
+
 let user;
 
 let confirm_modal = ref(false);
@@ -32,37 +33,7 @@ let email = ref("")
 let password = ref("")
 
 
-// eslint-disable-next-line no-unused-vars
-function login(email, password){
-  const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-    Username: email,
-    Password: password
-  });
-  const userData = {
-    Username: email,
-    Pool: userPool,
-  }
-  const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-  cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: function (result){
-      console.log("access Token: " + result.getAccessToken().getJwtToken())
-      console.log("id Token: " + result.getIdToken().getJwtToken())
-      console.log("refresh Token: " + result.getRefreshToken().getJwtToken())
-    },
-    onFailure: function (err){
-      console.log(err)
-    }
-  })
-}
-function signUp() {
-  userPool.signUp(email, password,signUp, null, null, (err, result)=>{
-    if(err) {
-      console.log(err)
-    }
-    user = result.usage;
-    console.log(user.username)
-  })
-}
+
 </script>
 
 <style scoped>
