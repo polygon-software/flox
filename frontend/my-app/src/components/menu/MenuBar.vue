b8ee646ba600bb8b8fc121db0e05429f884eae91
davwys <david.wyss@hotmail.ch>
2021-10-19 14:00:52 +0200

    Status login

--- frontend/my-app/src/components/menu/MenuBar.vue
+++ frontend/my-app/src/components/menu/MenuBar.vue
@@ -3,8 +3,12 @@
     <img
         alt="Polygon Software"
         src="https://media-exp1.licdn.com/dms/image/C4D0BAQEI1LFXsM4DVA/company-logo_200_200/0/1593964710523?e=2159024400&v=beta&t=k1qIEpVNRq-GBvW1fZt2SKvcuq59WL8J0IuLW0qMSG4"
-        style="height: 130px">
-    <h4 class="text-black">Hello World! Test Application is running.</h4>
+        style="height: 50px"
+        class="q-ma-sm"
+    >
+    <h5 class="text-black q-pa-none q-ma-md">
+      PolygonSoftware Template
+    </h5>
   </q-header>
 </template>
 
