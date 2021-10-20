804ed83935ea4c8386dbdf3daeff34d7734014b8
davwys <david.wyss@hotmail.ch>
2021-10-20 13:47:14 +0200

    Separate pages

--- frontend/my-app/src/pages/LoginPage.vue
+++ frontend/my-app/src/pages/LoginPage.vue
@@ -1,24 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <!-- Login Card -->
-    <q-card class="q-pa-md q-ma-md">
-      <LoginForm @submit="onLogin"/>
-    </q-card>
+  <q-layout view="lHh Lpr lFf" class="bg-grey-4">
+    <MenuBar :logged-in="$authService.isLoggedIn()"/>
+    <q-page class="flex flex-center">
+      <!-- Login Card -->
+      <q-card class="q-pa-md q-ma-md">
+        <LoginForm @submit="onLogin"/>
+      </q-card>
 
-    <!-- Signup Card -->
-    <q-card class="q-pa-md q-ma-md">
-      <SignupForm @submit="onSignup"/>
-    </q-card>
-  </q-page>
+      <!-- Signup Card -->
+      <q-card class="q-pa-md q-ma-md">
+        <SignupForm @submit="onSignup"/>
+      </q-card>
+    </q-page>
+  </q-layout>
 </template>
 
 <script setup lang="ts">
 import LoginForm from "../components/forms/LoginForm.vue"
 import SignupForm from "../components/forms/SignupForm.vue"
-import { inject } from 'vue'
+import MenuBar from "../components/menu/MenuBar"
+import {inject} from 'vue'
 
 const $authService: any = inject('$authService')
 
+/**
+ * Logs in the given user
+ * @param username {string} - the user's username
+ * @param password {string} - the user's password
+ */
 async function onLogin({username, password}: {username: string, password: string}){
   await $authService.value.login(username, password)
 }
@@ -26,8 +35,8 @@ async function onLogin({username, password}: {username: string, password: string
 
 /**
  * Registers a new user using the given data and opens the corresponding e-mail verification dialog
- * @param username {string} - The user's chosen username
- * @param email {string} - The user's e-mail address
+ * @param username {string} - the user's chosen username
+ * @param email {string} - the user's e-mail address
  * @param password {string} - the user's chosen password
  */
 async function onSignup({username, email, password}:{username: string, email: string, password:string}){
--- frontend/my-app/src/pages/MainPage.vue
+++ frontend/my-app/src/pages/MainPage.vue
@@ -1,23 +1,30 @@
 <template>
-  <q-page class="flex flex-center">
-    <!-- Test table for visualizing data fetched via GraphQL/Apollo -->
-    <div class="column">
-      <div class="row q-pa-md">
-        <SampleTable/>
-        <SampleForm/>
-      </div>
+  <q-layout view="lHh Lpr lFf" class="bg-grey-4">
+    <MenuBar :logged-in="$authService.isLoggedIn()"/>
+    <q-page class="flex flex-center">
+      <!-- Test table for visualizing data fetched via GraphQL/Apollo -->
+      <div class="column">
+        <div class="row q-pa-md">
+          <SampleTable/>
+          <SampleForm/>
+        </div>
 
-      <div class="q-pa-md">
-        <SampleSubscriptionTable/>
+        <div class="q-pa-md">
+          <SampleSubscriptionTable/>
+        </div>
       </div>
-    </div>
-  </q-page>
+    </q-page>
+  </q-layout>
 </template>
 
 <script setup lang="ts">
 import SampleTable from "../components/SampleTable"
 import SampleForm from "../components/SampleForm"
 import SampleSubscriptionTable from "../components/SampleSubscriptionTable"
+import MenuBar from "../components/menu/MenuBar"
+import {inject} from "vue";
+
+const $authService: any = inject('$authService')
 </script>
 
 <style scoped>
