9983a0de2ca84b468d335e75e24cc41451044ff8
davwys <david.wyss@hotmail.ch>
2021-10-21 13:13:25 +0200

    Set up new quasar project

--- frontend/my-app/src/App.vue
+++ frontend/my-app/src/App.vue
@@ -1,39 +1,10 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-<script setup lang="ts">
-import {useQuasar} from "quasar";
-import {AuthenticationService} from "@/services/AuthService";
-import {provide, ref} from "vue";
-import {ErrorService} from "@/services/ErrorService";
-import {setupRouter} from "@/router/route-helpers";
-
-const $q = useQuasar()
-
-// Error service
-const $errorService = ref(new ErrorService($q))
-provide("$errorService", $errorService)
-
-// Auth service
-const $authService = ref(new AuthenticationService($q, $errorService))
-provide("$authService", $authService)
-
-// Quasar
-provide("$q", $q)
-
-// Set up router
-setupRouter($authService.value)
-
+export default defineComponent({
+  name: 'App'
+})
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
