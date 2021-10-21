925c8c0287cf580617805363e47337884d4854e0
davwys <david.wyss@hotmail.ch>
2021-10-21 16:01:57 +0200

    Fix some auth issues

--- frontend/my-app/quasar.conf.js
+++ frontend/my-app/quasar.conf.js
@@ -30,7 +30,8 @@ module.exports = configure(function (ctx) {
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
       'i18n',
-      'router'
+      'router',
+      'apollo'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
