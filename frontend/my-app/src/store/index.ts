5dd83f75ed457c9d5d5faf85f2d34d31e9215a9e
davwys <david.wyss@hotmail.ch>
2021-10-21 14:57:01 +0200

    User module

--- frontend/my-app/src/store/index.ts
+++ frontend/my-app/src/store/index.ts
@@ -5,6 +5,7 @@ import {
   Store as VuexStore,
   useStore as vuexUseStore,
 } from 'vuex'
+import user from './user'
 
 // import example from './module-example'
 // import { ExampleStateInterface } from './module-example/state';
@@ -38,7 +39,7 @@ export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol('vuex-ke
 export default store(function (/* { ssrContext } */) {
   const Store = createStore<StateInterface>({
     modules: {
-      // example
+      user,
     },
 
     // enable strict mode (adds overhead!)
