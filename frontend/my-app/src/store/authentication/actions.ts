c6a17105add39c2694c512e7f607fb2435399092
davwys <david.wyss@hotmail.ch>
2021-10-21 15:23:27 +0200

    TS fixes

--- frontend/my-app/src/store/authentication/actions.ts
+++ frontend/my-app/src/store/authentication/actions.ts
@@ -1,4 +1,11 @@
-/*
-export function someAction (context) {
-}
-*/
+import { ActionTree } from 'vuex';
+import { StateInterface } from '../index';
+import { AuthStateInterface } from './state';
+
+const actions: ActionTree<AuthStateInterface, StateInterface> = {
+  someAction (/* context */) {
+    // your code
+  }
+};
+
+export default actions;
--- frontend/my-app/src/store/authentication/mutations.ts
+++ frontend/my-app/src/store/authentication/mutations.ts
@@ -1,4 +1,10 @@
-/*
-export function someMutation (state) {
-}
-*/
+import { MutationTree } from 'vuex';
+import { AuthStateInterface } from './state';
+
+const mutations: MutationTree<AuthStateInterface> = {
+  someMutation (/* state: ExampleStateInterface */) {
+    // your code
+  }
+};
+
+export default mutations;
