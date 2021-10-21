2af38d28cb9e780837ffae37f669a1c1fc3eebde
davwys <david.wyss@hotmail.ch>
2021-10-21 16:19:35 +0200

    Fixes in AuthService

--- frontend/my-app/src/store/authentication/actions.ts
+++ frontend/my-app/src/store/authentication/actions.ts
@@ -2,6 +2,8 @@ import { ActionTree } from 'vuex';
 import { StateInterface } from '../index';
 import { AuthStateInterface } from './state';
 
+// TODO
+
 const actions: ActionTree<AuthStateInterface, StateInterface> = {
   // someAction (/* context */) {
   //   // your code
--- frontend/my-app/src/store/authentication/mutations.ts
+++ frontend/my-app/src/store/authentication/mutations.ts
@@ -1,10 +1,18 @@
 import { MutationTree } from 'vuex';
 import { AuthStateInterface } from './state';
+import {CognitoUser, CognitoUserSession} from 'amazon-cognito-identity-js';
+
+function setUserSession (state: AuthStateInterface, payload: CognitoUserSession) {
+  state.userSession = payload
+}
+
+function setCognitoUser (state: AuthStateInterface, payload: CognitoUser) {
+  state.cognitoUser = payload
+}
 
 const mutations: MutationTree<AuthStateInterface> = {
-  // someMutation (/* state: ExampleStateInterface */) {
-  //   // your code
-  // }
+  setUserSession,
+  setCognitoUser,
 };
 
 export default mutations;
