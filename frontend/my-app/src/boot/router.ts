c87acd02927f0c798f232d2fb43b99569fde857d
davwys <david.wyss@hotmail.ch>
2021-10-21 17:48:35 +0200

    more fixes, works now

--- frontend/my-app/src/boot/router.ts
+++ frontend/my-app/src/boot/router.ts
@@ -3,9 +3,11 @@ import ROUTES from '../router/routes'
 
 export default boot(({ router, store }) => {
   router.beforeEach((to) => {
-
+    console.log('blubb!')
     // Verify valid authentication
     if(to.path !== ROUTES.LOGIN.path && !store.getters['authentication/getLoggedInStatus']){
+      //next({ name: 'account-signin', query: { next: to.fullPath } })
+
       return Promise.reject({ url: ROUTES.LOGIN.path })
     }
   })
