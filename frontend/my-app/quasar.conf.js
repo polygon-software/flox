572cb7be5d7836fe0798f175d0664eefbf221c4a
davwys <david.wyss@hotmail.ch>
2021-10-25 09:16:42 +0200

    Add dialog£

--- frontend/my-app/quasar.conf.js
+++ frontend/my-app/quasar.conf.js
@@ -56,6 +56,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      env: require('dotenv').config().parsed,
 
       // transpile: false,
       // publicPath: '/',
@@ -103,7 +104,9 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Dialog'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
