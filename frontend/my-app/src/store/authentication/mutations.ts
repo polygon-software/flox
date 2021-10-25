725b20a490cc0e9a0371b5a5719f9ad06f13be2c
davwys <david.wyss@hotmail.ch>
2021-10-25 16:44:02 +0200

    More cookie stuff, persist tokens

--- frontend/my-app/src/store/authentication/mutations.ts
+++ frontend/my-app/src/store/authentication/mutations.ts
@@ -6,41 +6,53 @@ import {Cookies} from 'quasar';
 function setUserSession (state: AuthStateInterface, payload: CognitoUserSession) {
   state.userSession = payload
 
-  // TODO lul
-  setPersistedState(state, state)
+  // Persist tokens TODO which ones make sense?
+  const accessToken = state.userSession.getAccessToken()
+  const idToken = state.userSession.getIdToken()
+  const refreshToken = state.userSession.getRefreshToken()
+
+  // Persist tokens to cookies
+  setPersistedData(state, {
+    accessToken,
+    idToken,
+    refreshToken
+  })
 }
 
 function setCognitoUser (state: AuthStateInterface, payload: CognitoUser) {
   state.cognitoUser = payload
-
-  // TODO lul
-  setPersistedState(state, state)
 }
 
 // Based on cookie-persisted store state, re-set store to that state
-function setPersistedState (state: AuthStateInterface, payload: AuthStateInterface){
+function setPersistedData (state: AuthStateInterface, payload: any){
   console.log('Called setPersistedState with', state, 'and payload', payload)
   // Set cookie when SSR fetch is done (ie. only browser can set a cookie)
   if (!process.env.SERVER && payload) { // TODO do we even need &&payload ?
-    console.log('Setting cookie to', JSON.stringify(payload))
     // TODO enable secure for production
-    Cookies.set(
-      'authentication',
-      JSON.stringify(payload),
-      {expires: 3, secure: false}
-    )
-  }
+    Object.keys(payload).forEach((key: string) => {
 
-  console.log('state old:', state)
-  // Copy payload to state
-  state = {...payload} // TODO for each key?
-  console.log('State new:', state)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const data: any = payload[key]
+      console.log('Data:', data)
+      Cookies.set(
+        `authentication.${key}`,
+        JSON.stringify(data),
+        {expires: 3, secure: false}
+      )
+    })
+  }
+  //
+  // console.log('state old:', state)
+  // // Copy payload to state
+  // state = {...payload} // TODO for each key
+  // console.log('State new:', state) TODO get...
 }
 
 const mutations: MutationTree<AuthStateInterface> = {
   setUserSession,
   setCognitoUser,
-  setPersistedState
+  setPersistedData
 };
 
 export default mutations;
