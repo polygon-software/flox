6e1816fcf6df788d2c8e75e5914adbef301daeb9
davwys <david.wyss@hotmail.ch>
2021-10-25 09:16:42 +0200

    Add dialog£

--- frontend/my-app/src/App.vue
+++ frontend/my-app/src/App.vue
@@ -2,28 +2,23 @@
   <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import {AuthenticationService} from './services/AuthService';
+import {provide, ref} from 'vue';
+import {ErrorService} from './services/ErrorService';
+import {useQuasar} from 'quasar';
 
-export default defineComponent({
-  name: 'App'
-})
-// import {AuthenticationService} from './services/AuthService';
-// import {provide, ref} from 'vue';
-// import {ErrorService} from './services/ErrorService';
-// import {useQuasar} from 'quasar';
-//
-// const $q = useQuasar()
-//
-// // Error service
-// const $errorService = ref(new ErrorService($q))
-// provide('$errorService', $errorService)
-//
-// // Auth service
-// const $authService = ref(new AuthenticationService($q, $errorService))
-// provide<AuthenticationService>('$authService', $authService)
-//
-// // Quasar
-// provide('$q', $q)
+const $q = useQuasar()
+
+// Error service
+const $errorService = ref(new ErrorService($q))
+provide('$errorService', $errorService)
+
+// Auth service
+const $authService = ref(new AuthenticationService($q, $errorService))
+provide<AuthenticationService>('$authService', $authService)
+
+// Quasar
+provide('$q', $q)
 
 </script>
