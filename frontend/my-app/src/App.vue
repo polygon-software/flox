7488bc612b40cb4a93e56688ed4e3a2b5f5508c3
davwys <david.wyss@hotmail.ch>
2021-10-25 12:50:21 +0200

    Apollo config fixes

--- frontend/my-app/src/App.vue
+++ frontend/my-app/src/App.vue
@@ -7,6 +7,8 @@ import {AuthenticationService} from './services/AuthService';
 import {provide, ref} from 'vue';
 import {ErrorService} from './services/ErrorService';
 import {useQuasar} from 'quasar';
+import {RouterService} from 'src/services/RouterService';
+import {routerInstance} from 'boot/router';
 
 const $q = useQuasar()
 
@@ -17,6 +19,10 @@ provide('$errorService', $errorService)
 // Auth service
 const $authService = ref(new AuthenticationService($q, $errorService))
 provide<AuthenticationService>('$authService', $authService)
+
+// Router service
+const $routerService = ref(new RouterService(routerInstance))
+provide<RouterService>('$routerService', $routerService)
 
 // Quasar
 provide('$q', $q)
--- frontend/my-app/src/boot/apollo.ts
+++ frontend/my-app/src/boot/apollo.ts
@@ -1,53 +1,17 @@
-import {ApolloClient, createHttpLink, InMemoryCache, split /*, createHttpLink */} from '@apollo/client/core'
+import {ApolloClient} from '@apollo/client/core'
 import { ApolloClients } from '@vue/apollo-composable'
 import { boot } from 'quasar/wrappers'
 import { getClientOptions } from 'src/apollo'
-import {WebSocketLink} from '@apollo/client/link/ws';
-import {getMainDefinition} from '@apollo/client/utilities';
 
 export default boot(
   ({ app }) => {
-    // HTTP link for GraphQL (Queries/Mutations)
-    const httpLink = createHttpLink({
-      // GraphQL API Link
-      uri: 'http://localhost:3000/graphql',
-    })
 
-    // WebSocket link for GraphQL (Subscriptions)
-    const wsLink = new WebSocketLink({
-      uri: 'ws://localhost:3000/graphql-websocket',
-      options: {
-        reconnect: true
-      }
-    });
-
-    // Depending on operation, use correct link
-    const link = split(
-      // split based on operation type
-      ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-          definition.kind === 'OperationDefinition' &&
-          definition.operation === 'subscription'
-        );
-      },
-      wsLink,
-      httpLink
-    );
 
     // Default client.
     const options = getClientOptions(/* {app, router ...} */)
 
     // Apollo Client setup
-    const apolloClient = new ApolloClient({
-      ...options,
-      link: link,
-      cache: new InMemoryCache({
-        addTypename: false, // We disable auto-adding of __typename property, as this breaks mutations expecting
-                            // an object variable. Instead, we manually add __typename in QUERIES/MUTATIONS.ts where
-                            // appropriate. This can be changed in case Apollo implements better behavior for this.
-      })
-    })
+    const apolloClient = new ApolloClient(options)
 
     const apolloClients: Record<string, ApolloClient<unknown>> = {
       default: apolloClient,
