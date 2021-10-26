fde06627d77c4760c7c402e37aad9850b5f5cdcc
davwys <david.wyss@hotmail.ch>
2021-10-26 10:15:48 +0200

    More cleanup, prepare for PR

--- frontend/my-app/src/data/MUTATIONS.ts
+++ frontend/my-app/src/data/MUTATIONS.ts
@@ -4,8 +4,7 @@ import {MutationTypes} from './DATA-DEFINITIONS';
 /**
  * This file contains all valid GraphQL mutations. A mutation is structure as follows
  * (see also DATA-DEFINITIONS.ts):
- * - mutation: the actual GraphQL mutation. Add __typename to the variables if the mutation only UPDATES data.
- * TODO __typename is also needed for create, why....
+ * - mutation: the actual GraphQL mutation. Add __typename to the variables if the mutation UPDATES or CREATES data.
  * - tables: list of affected tables; when the mutation is executed, the corresponding queries are re-fetched.
  * - type: the mutation's type (either CREATE, DELETE or UPDATE); this determines cache handling
  * - cacheLocation: the actual GraphQL mutation's name (since cached data will be stored there)
