cb5c84e88a6563fa0dfd0bf7c31e6f15afce932e
davwys <david.wyss@hotmail.ch>
2021-10-28 14:52:14 +0200

    Public key change

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,16 +1,7 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import * as jwkToPem from 'jwk-to-pem';
-
-const keyObject = {
-  alg: 'RS256',
-  e: 'AQAB',
-  kid: 'yfQu0/N8wCd0EJKkTauWkU2a+15bYYWrJfPEUBJwCWQ=',
-  kty: 'RSA',
-  n: 'pc-kq76um33KpXwKSLCJF6qaUFy-s25NssKqDDBrrS0YuPsjOU81jlxZ50aRJuKZVZW5dtPpxsh8EI1YqT7vBuujN0A94LIvMg5GaRpBIy7zzsAHVvcFP6kYQ2cSF6WuCxJtxH7jPi0-fIwbbGR0-5RFLdsDK--XmCftiQfOFeAJ2wX99GkeiceWT7gRZ3yOZd6Gp-9eYVxZdxsK70bI_WnKTFdxydjKmlLZ8jAqEUbB1jXHF5soc7YChrubFWI1qXSjRCzD0SlBFsa6S2bXBkor9T5Vws0X9kf9SO7vT1YHhvo1OneFq74w_vyM3JY7eSUrxXHJ5S7sOO4hPD3rMw',
-  use: 'sig',
-};
+import { passportJwtSecret } from 'jwks-rsa';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -18,7 +9,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwkToPem(keyObject),
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri:
+          'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_DGPNZZeuX/.well-known/jwks.json',
+      }),
     });
   }
 
