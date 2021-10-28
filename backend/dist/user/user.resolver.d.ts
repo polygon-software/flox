5dd657beba67bdc29d5eaadd60080b799e3df7f9
davwys <david.wyss@hotmail.ch>
2021-10-28 08:09:30 +0200

    Rename, add dev command to package

---
+++ backend/dist/user/user.resolver.d.ts
@@ -0,0 +1,18 @@
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { GetUserArgs } from './dto/args/get-user.args';
+import { DeleteUserInput } from './dto/input/delete-user.input';
+import { User } from './entities/user.entity';
+import { GetUsersArgs } from './dto/args/get-users.args';
+export declare class UserResolver {
+    private readonly usersService;
+    constructor(usersService: UserService);
+    getUsers(getUsersArgs: GetUsersArgs): Promise<User[]>;
+    getAllUsers(): Promise<User[]>;
+    getUser(getUserArgs: GetUserArgs): Promise<User>;
+    create(createUserInput: CreateUserInput): Promise<User>;
+    update(updateUserInput: UpdateUserInput): Promise<User>;
+    remove(deleteUserInput: DeleteUserInput): Promise<User>;
+    userAdded(): AsyncIterator<unknown, any, undefined>;
+}
