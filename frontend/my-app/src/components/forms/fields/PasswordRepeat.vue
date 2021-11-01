610a1ea5a45d54659d7afb135e37a5ffc74695dc
davwys <david.wyss@hotmail.ch>
2021-11-01 11:15:10 +0100

    Fix page change in forms

--- frontend/my-app/src/components/forms/fields/PasswordRepeat.vue
+++ frontend/my-app/src/components/forms/fields/PasswordRepeat.vue
@@ -34,7 +34,7 @@
 </template>
 
 <script setup lang="ts">
-import {computed, ref, watch} from 'vue';
+import {ref, watch} from 'vue';
 import {IS_VALID_PASSWORD} from 'src/data/RULES';
 
 /**
--- frontend/my-app/src/helpers/form-helpers.ts
+++ frontend/my-app/src/helpers/form-helpers.ts
@@ -1,27 +1,49 @@
 import {computed, defineEmits, Ref, ref} from 'vue';
 import {FIELDS} from 'src/data/FIELDS';
 
-
-
+// The current step within a multi-page form
 const step: Ref<number> = ref(1)
+
+// The entered values
 const values: Ref<Record<string, any>> = ref({})
+
+// Events
 const emit = defineEmits(['submit'])
+
+// Page definition (Should get overwritten by each form)
+const pages: Ref<Record<string, any>[]> = ref([])
 
 /**
  * Determines whether the current page is filled with valid data
  * (used to determine whether to allow going to next step within form)
  */
 const pageValid = computed(() => {
+  // If page structure is not built yet, page can't be valid
+  if(pages.value.length === 0) return false
 
-  // Validate each field by its "rules" definition
-  const res = Object.keys(values.value).every((key)=>{
-    console.log('KEY:', key, key.toUpperCase())
+  console.log('Pages built, length', pages.value.length)
+
+  // Get keys that should exist in 'values' for this page
+  const pageKeys: string[] = []
+  // Offset by 1, since step starts at 1
+  const pageFields = pages.value[step.value - 1].fields
+  pageFields.forEach((field: Record<string, any>) => {
+    pageKeys.push(field.key)
+  })
+  console.log('Keys for this page:', pageKeys) // TODO remove
+
+
+  // Validate each field by its "rules" attribute
+  return pageKeys.every((key)=>{
+    console.log('CHECKING', key)
+    // If no value present at all, stop check
+    if(!values.value[key]) return false
+
     return FIELDS[key.toUpperCase()].attributes.rules.every((rule: (valueElement: any)=>boolean)=>{
+      // If the rule returns true, it is fulfilled (otherwise, it will return an error message)
       return typeof rule(values.value[key]) === 'boolean'
     })
   })
-
-  return res && values.value['email'] !== undefined
 })
 
 /**
@@ -45,6 +67,7 @@ export {
   step,
   values,
   emit,
+  pages,
   // Computed
   pageValid,
   // Methods
