63f133f1a50137f14c28bff7042841a31a17e512
davwys <david.wyss@hotmail.ch>
2021-11-02 15:40:05 +0100

    Helper setup beginning

---
+++ shared/helpers/schema-helpers.ts
@@ -0,0 +1,14 @@
+import * as Joi from "joi";
+
+/**
+ * This file contains helper functions related to creating Joi schemas
+ */
+
+function joinSchemas(schemas: Array<Joi.Schema>){
+    console.log("join schemas", schemas)
+}
+
+
+export {
+    joinSchemas,
+}
--- shared/schemas/AuctionableItem.ts
+++ shared/schemas/AuctionableItem.ts
@@ -1,7 +1,5 @@
-import * as Joi from "joi";
 import {BuyableItem} from "./BuyableItem";
 import {TimeableItem} from "./TimeableItem";
-
 
 // An item that can be timed and bought
 export const AuctionableItem = TimeableItem.concat(BuyableItem);
