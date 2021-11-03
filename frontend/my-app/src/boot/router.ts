69cb19e77c6378621477d8ca00c99069dcb8d4d9
davwys <david.wyss@hotmail.ch>
2021-11-03 18:01:59 +0100

    Change

--- frontend/my-app/src/boot/router.ts
+++ frontend/my-app/src/boot/router.ts
@@ -1,5 +1,5 @@
 import { boot } from 'quasar/wrappers'
-// import ROUTES from '../router/routes'
+import ROUTES from '../router/routes'
 import {Router} from 'vue-router';
 import {root} from 'src/store';
 
@@ -10,16 +10,16 @@ export default boot(({ router,store}) => {
   // Get auth module within store (useAuth not working here)
   const $authStore = root.context(store).modules.authModule
   routerInstance = router
-  // router.beforeEach((to) => {
+  router.beforeEach((to) => {
   const loggedIn = $authStore.getters.getLoggedInStatus()//   // Verify valid authentication
   console.log('Login?', loggedIn)
-  //   if(to.path !== ROUTES.LOGIN.path && !loggedIn){
-  //     return(ROUTES.LOGIN)
-  //   } else if(to.path === ROUTES.LOGIN.path && loggedIn){
-  //     // If user is logged in and trying to log in, redirect to main page
-  //     return(ROUTES.MAIN)
-  //   }
-  // })
+    // if(to.path !== ROUTES.LOGIN.path && !loggedIn){
+    //   return(ROUTES.LOGIN)
+    // } else if(to.path === ROUTES.LOGIN.path && loggedIn){
+    //   // If user is logged in and trying to log in, redirect to main page
+    //   return(ROUTES.MAIN)
+    // }
+  })
 })
 
 // Router instance for use in Vue components
