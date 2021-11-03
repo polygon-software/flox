b31761cbe3b8cc214787668d32170e4c53640641
davwys <david.wyss@hotmail.ch>
2021-11-03 10:38:01 +0100

    Disable routing for now

--- frontend/my-app/src/boot/router.ts
+++ frontend/my-app/src/boot/router.ts
@@ -1,23 +1,23 @@
 import { boot } from 'quasar/wrappers'
-import ROUTES from '../router/routes'
+// import ROUTES from '../router/routes'
 import {Router} from 'vue-router';
-import {useAuth} from 'src/store/authentication';
+// import {useAuth} from 'src/store/authentication';
 
 let routerInstance: Router
 
 export default boot(({ router }) => {
-  const $authStore = useAuth()
-
-  routerInstance = router
-  router.beforeEach((to) => {
-    // Verify valid authentication
-    if(to.path !== ROUTES.LOGIN.path && !$authStore.getters.getLoggedInStatus()){
-      return(ROUTES.LOGIN)
-    } else if(to.path === ROUTES.LOGIN.path && !$authStore.getters.getLoggedInStatus()){
-      // If user is logged in and trying to log in, redirect to main page
-      return(ROUTES.MAIN)
-    }
-  })
+  // const $authStore = useAuth()
+  // TODO
+  // routerInstance = router
+  // router.beforeEach((to) => {
+  //   // Verify valid authentication
+  //   if(to.path !== ROUTES.LOGIN.path && !$authStore.getters.getLoggedInStatus()){
+  //     return(ROUTES.LOGIN)
+  //   } else if(to.path === ROUTES.LOGIN.path && !$authStore.getters.getLoggedInStatus()){
+  //     // If user is logged in and trying to log in, redirect to main page
+  //     return(ROUTES.MAIN)
+  //   }
+  // })
 })
 
 // Router instance for use in Vue components
