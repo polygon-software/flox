d6f692c1ce3249477ca0d40aef95f59ce919bdd1
davwys <david.wyss@hotmail.ch>
2021-11-04 15:17:22 +0100

    Cleanup store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,32 +2,6 @@ import { store } from 'quasar/wrappers'
 import {createStore, Module} from 'vuex-smart-module'
 import authModule from './authentication'
 
-// import example from './module-example'
-// import { ExampleStateInterface } from './module-example/state';
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-// TODO...
-// export interface StateInterface {
-//   authentication: AuthStateInterface
-// }
-
-// provide typings for `this.$store`
-// declare module '@vue/runtime-core' {
-//   interface ComponentCustomProperties {
-//     $store: VuexStore<StateInterface>
-//   }
-// }
-
-// provide typings for `useStore` helper
-// export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol('vuex-key')
 export const root = new Module({
     modules: {
       authModule,
