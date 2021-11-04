a577c56af7c8062f803c072e77e89715a721895b
Johann Schwabe <Johann.schwabe@uzh.ch>
2021-11-04 09:26:50 +0100

    fixed some stuff

--- frontend/my-app/src/boot/auth.ts
+++ frontend/my-app/src/boot/auth.ts
@@ -5,8 +5,9 @@ import {
 } from 'amazon-cognito-identity-js';
 import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js'
 import {root} from 'src/store';
+import {Cookies} from 'quasar';
 
-export default boot(({ store}) => {
+export default boot(({ store, ssrContext}) => {
   console.log('auth boot')
   // Set up authentication user pool
   const poolSettings:ICognitoUserPoolData = {
@@ -16,7 +17,28 @@ export default boot(({ store}) => {
   const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolSettings)
   const $authStore = root.context(store).modules.authModule
   $authStore.mutations.setUserPool(userPool)
-  // const cognitoUser = CognitoIdentityServiceProvider
+  let cognitoUser;
+  if(process.env.SERVER && ssrContext){
+    // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-unsafe-assignment
+    const XMLHttpRequest = require('xhr2');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const xht = new XMLHttpRequest() as XMLHttpRequest
+    console.log(ssrContext)
+    const cookies = Cookies.parseSSR(ssrContext)
+    console.log(cookies)
+    const accessToken = cookies.get('authentication.accessToken')
+    console.log(accessToken)
+    xht.open('POST','https://cognito-idp.eu-central-1.amazonaws.com/')
+    xht.setRequestHeader('X-Amz-Target', 'AWSCognitoIdentityProviderService.GetUser');
+    xht.setRequestHeader('Accept', 'application/json');
+    xht.setRequestHeader('Content-Type', 'application/x-amz-json-1.1');
+    xht.onreadystatechange = function (){
+      console.log(xht.responseText)
+    }
+    xht.send(JSON.stringify({'AccessToken':accessToken}))
+  } else {
+    // cognitoUser = userPool.getCurrentUser()
+  }
   // if(cognitoUser){
   //   // Auto-log in user
   //   cognitoUser.getSession(function(err: Error|null, session: CognitoUserSession|null) {
