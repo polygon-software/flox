b728e4eb44f316864481b832debee6fb982eca05
Johann Schwabe <Johann.schwabe@uzh.ch>
2021-11-04 11:45:56 +0100

    session is assembled

--- frontend/my-app/src/boot/auth.ts
+++ frontend/my-app/src/boot/auth.ts
@@ -1,56 +1,117 @@
 import { boot } from 'quasar/wrappers'
 import {
-
+  CognitoUser,
   ICognitoUserPoolData,
 } from 'amazon-cognito-identity-js';
 import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js'
 import {root} from 'src/store';
 import {Cookies} from 'quasar';
+import {CognitoAccessToken, CognitoRefreshToken} from 'amazon-cognito-auth-js';
 
-export default boot(({ store, ssrContext}) => {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/ban-ts-comment
+function _interopRequireDefault(obj:unknown) { // @ts-ignore
+  return obj && obj.__esModule ? obj : { 'default': obj }; }
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const _CognitoUserSession = _interopRequireDefault(require('amazon-cognito-identity-js/lib/CognitoUserSession'));
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const _CognitoIdToken = _interopRequireDefault(require('amazon-cognito-identity-js/lib/CognitoIdToken'));
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const _CognitoAccessToken = _interopRequireDefault(require('amazon-cognito-identity-js/lib/CognitoAccessToken'));
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const _CognitoRefreshToken = _interopRequireDefault(require('amazon-cognito-identity-js/lib/CognitoRefreshToken'));
+
+export default boot(async ({ store, ssrContext}) => {
   console.log('auth boot')
+
+  const $authStore = root.context(store).modules.authModule
+
   // Set up authentication user pool
   const poolSettings:ICognitoUserPoolData = {
     UserPoolId: process.env.VUE_APP_USER_POOL_ID ?? '',
     ClientId: process.env.VUE_APP_USER_POOL_CLIENT_ID ?? '',
   };
   const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolSettings)
-  const $authStore = root.context(store).modules.authModule
   $authStore.mutations.setUserPool(userPool)
-  let cognitoUser;
+
+  let cognitoUser: CognitoUser|undefined;
   if(process.env.SERVER && ssrContext){
     // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-unsafe-assignment
     const XMLHttpRequest = require('xhr2');
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     const xht = new XMLHttpRequest() as XMLHttpRequest
-    console.log(ssrContext)
     const cookies = Cookies.parseSSR(ssrContext)
-    console.log(cookies)
     const accessToken = cookies.get('authentication.accessToken')
-    console.log(accessToken)
+    if(!accessToken){return}
+    console.log('AccessToken:', accessToken)
     xht.open('POST','https://cognito-idp.eu-central-1.amazonaws.com/')
     xht.setRequestHeader('X-Amz-Target', 'AWSCognitoIdentityProviderService.GetUser');
     xht.setRequestHeader('Accept', 'application/json');
     xht.setRequestHeader('Content-Type', 'application/x-amz-json-1.1');
-    xht.onreadystatechange = function (){
-      console.log(xht.responseText)
-    }
-    xht.send(JSON.stringify({'AccessToken':accessToken}))
+    await new Promise<void>(((resolve) => {
+      xht.onreadystatechange = function (){
+        const responseObject: Record<string, unknown> = JSON.parse(xht.response) as Record<string, unknown>
+
+        if(!xht.response || xht.status !== 200){
+          console.log('rejecting with xht.status: ' + xht.status.toString())
+          // reject()
+          return
+        }
+        cognitoUser = new CognitoUser({
+          Pool: userPool,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          Username: responseObject.Username as string,
+        })
+        $authStore.mutations.setCognitoUser(cognitoUser)
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+        const idToken = new _CognitoIdToken['default']({
+          IdToken: cookies.get('authentication.idToken')
+        });
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+        const accessToken = new _CognitoAccessToken['default']({
+          AccessToken: cookies.get('authentication.accessToken')
+        });
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+        const refreshToken = new _CognitoRefreshToken['default']({
+          RefreshToken: cookies.get('authentication.refreshToken')
+        });
+        const sessionData = {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          IdToken: idToken,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          AccessToken: accessToken,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          RefreshToken: refreshToken,
+          ClockDrift: 0
+        };
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+        const cachedSession = new _CognitoUserSession['default'](sessionData);
+        $authStore.mutations.setUserSession(cachedSession)
+        resolve()
+      }
+      xht.send(JSON.stringify({'AccessToken':accessToken}))
+    }))
+
   } else {
-    // cognitoUser = userPool.getCurrentUser()
+    cognitoUser = userPool.getCurrentUser() || undefined
+    if(cognitoUser){
+      $authStore.mutations.setCognitoUser(cognitoUser)
+      cognitoUser.getSession(function(err: Error|null, session: AmazonCognitoIdentity.CognitoUserSession|undefined) {
+        if (!err) {
+          $authStore.mutations.setUserSession(session)
+        }
+      })
+    }
+
   }
-  // if(cognitoUser){
-  //   // Auto-log in user
-  //   cognitoUser.getSession(function(err: Error|null, session: CognitoUserSession|null) {
-  //     if (!err) {
-  //       const cognitoUserSession = session;
-  //       // Set in store
-  //       store.commit('authentication/setCognitoUser', cognitoUser)
-  //       store.commit('authentication/setUserSession', cognitoUserSession)
-  //       console.log("user set")
-  //
-  //     }
-  //   });
-  // }
+
+
 })
 
--- frontend/my-app/src/boot/router.ts
+++ frontend/my-app/src/boot/router.ts
@@ -1,25 +1,26 @@
 import { boot } from 'quasar/wrappers'
-// // import ROUTES from '../router/routes'
+import ROUTES from '../router/routes'
 import {Router} from 'vue-router';
-// import {root} from 'src/store';
+import {root} from 'src/store';
 //
 let routerInstance: Router
 //
-export default boot(({ router}) => {
+export default boot(({ router, store}) => {
 //
 //   // Get auth module within store (useAuth not working here)
-//   // const $authStore = root.context(store).modules.authModule
+  const $authStore = root.context(store).modules.authModule
   routerInstance = router
-//   // router.beforeEach((to) => {
-//     // const loggedIn = $authStore.getters.getLoggedInStatus()//   // Verify valid authentication
-//     // console.log('Login?', loggedIn)
-//     // if(to.path !== ROUTES.LOGIN.path && !loggedIn){
-//     //   return(ROUTES.LOGIN)
-//     // } else if(to.path === ROUTES.LOGIN.path && loggedIn){
-//     //   // If user is logged in and trying to log in, redirect to main page
-//     //   return(ROUTES.MAIN)
-//     // }
-//   // })
+  router.beforeEach((to) => {
+    const loggedIn = $authStore.getters.getLoggedInStatus()//   // Verify valid authentication
+    console.log($authStore.getters.getUserSession())//   // Verify valid authentication
+    console.log('Login?', loggedIn)
+    if(to.path !== ROUTES.LOGIN.path && !loggedIn){
+      return(ROUTES.LOGIN)
+    } else if(to.path === ROUTES.LOGIN.path && loggedIn){
+      // If user is logged in and trying to log in, redirect to main page
+      return(ROUTES.MAIN)
+    }
+  })
 })
 //
 // // Router instance for use in Vue components
