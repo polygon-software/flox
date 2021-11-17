7f2fa33a5fdb3ee0b0ac76c1ac89d588ac1a8b61
Elexa Heggli <elexa.heggli@gmx.ch>
2021-11-17 14:10:39 +0100

    finished quick redirection page - not best css ever, but i thought i'd leave i atm - no actual redirection yet bc we don't have the page to redirect to yet

---
+++ frontend/src/pages/SuccessAndRedirect.vue
@@ -0,0 +1,11 @@
+<template>
+    <q-page class="justify-xs-center">
+      <q-banner inline-actions rounded class="col-8 bg-green text-white q-mx-xl q-mt-xl text-center"> Your application was successful.</q-banner>
+      <p class="col text-center q-mt-md"> You will be redirected shortly... </p>
+    </q-page>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
