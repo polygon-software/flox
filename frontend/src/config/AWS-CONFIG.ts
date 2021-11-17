0c7d240a2cb690049c5843b2c7543508ff005881
davwys <david.wyss@hotmail.ch>
2021-11-17 11:16:35 +0100

    Config cleanup

---
+++ frontend/src/config/AWS-CONFIG.ts
@@ -0,0 +1,12 @@
+/**
+ * This file contains the AWS Configuration.
+ * TODO NOTE: actual production keys should be moved outside this file to .env files!
+ */
+
+const CONFIG = {
+  accessKeyId: '***REMOVED***',
+  secretAccessKey: 'ZHhTQgfGUFT6uPk8wwi//L7AiuN9ETliaLhFRsEH',
+  region: 'eu-central-1',
+}
+
+export {CONFIG}
