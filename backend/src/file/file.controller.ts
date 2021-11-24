fdcb1cb654f0aea3b9c1367efd69dfe35a1203c1
davwys <david.wyss@hotmail.ch>
2021-11-24 10:12:21 +0100

    Add endpoint

--- backend/src/file/file.controller.ts
+++ backend/src/file/file.controller.ts
@@ -14,12 +14,12 @@ export class FileController {
   constructor(private readonly taskService: FileService) {}
 
   @Public()
-  @Post('/uploadFile')
-  async uploadFile(
+  @Post('/uploadPublicFile')
+  async uploadPublicFile(
     @Req() req: fastify.FastifyRequest,
     @Res() res: fastify.FastifyReply<any>,
   ): Promise<any> {
-    //Check request is multipart
+    // Verify that request is multipart
     if (!req.isMultipart()) {
       res.send(new BadRequestException('File expected on this endpoint'));
       return;
@@ -27,6 +27,26 @@ export class FileController {
     const file = await req.file();
     const file_buffer = await file.toBuffer();
     const new_file = await this.taskService.uploadPublicFile(
+      file_buffer,
+      file.filename,
+    );
+    res.send(new_file);
+  }
+
+  @Public() // TODO change?
+  @Post('/uploadPrivateFile')
+  async uploadPrivateFile(
+    @Req() req: fastify.FastifyRequest,
+    @Res() res: fastify.FastifyReply<any>,
+  ): Promise<any> {
+    // Verify that request is multipart
+    if (!req.isMultipart()) {
+      res.send(new BadRequestException('File expected on this endpoint'));
+      return;
+    }
+    const file = await req.file();
+    const file_buffer = await file.toBuffer();
+    const new_file = await this.taskService.uploadPrivateFile(
       file_buffer,
       file.filename,
     );
--- backend/src/file/file.resolver.ts
+++ backend/src/file/file.resolver.ts
@@ -3,6 +3,8 @@ import PublicFile from './entities/public_file.entity';
 import { FileService } from './file.service';
 import { Public } from '../auth/auth.guard';
 import { GetPublicFileArgs } from './dto/get-public-file.args';
+import { GetPrivateFileArgs } from './dto/get-private-file.args';
+import PrivateFile from './entities/private_file.entity';
 
 @Resolver(() => PublicFile)
 export class FileResolver {
@@ -10,7 +12,17 @@ export class FileResolver {
 
   @Public()
   @Query(() => PublicFile, { name: 'getPublicFile' })
-  async getPublicFile(@Args() getFileArgs: GetPublicFileArgs): Promise<PublicFile> {
-    return await this.fileService.getPublicFile(getFileArgs);
+  async getPublicFile(
+    @Args() getPublicFileArgs: GetPublicFileArgs,
+  ): Promise<PublicFile> {
+    return await this.fileService.getPublicFile(getPublicFileArgs);
+  }
+
+  @Public() // TODO restrict to files owned by me
+  @Query(() => PrivateFile, { name: 'getPrivateFile' })
+  async getPrivateFile(
+    @Args() getPrivateFileArgs: GetPrivateFileArgs,
+  ): Promise<PrivateFile> {
+    return await this.fileService.getPrivateFile(getPrivateFileArgs);
   }
 }
