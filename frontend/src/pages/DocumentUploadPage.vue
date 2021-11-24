a7cb6a8819025ffbe33c58b079b45774fb0e8ef4
ramizee <43523869+ramizee@users.noreply.github.com>
2021-11-24 15:13:33 +0100

    documentuploadform deleted, used genericform

--- frontend/src/pages/DocumentUploadPage.vue
+++ frontend/src/pages/DocumentUploadPage.vue
@@ -2,7 +2,11 @@
   <q-page class="flex flex-center">
     <div class="column">
       <q-card class="q-pa-md">
-        <DocumentUploadForm :pages="pages"/>
+        <GenericForm
+          :finish-label="$t('finish_signup')"
+          :pages="pages"
+          @submit="onSubmit"
+        />
       </q-card>
     </div>
   </q-page>
@@ -11,7 +15,14 @@
 <script setup lang="ts">
 import {FIELDS} from 'src/data/FIELDS';
 import {i18n} from 'boot/i18n';
-import DocumentUploadForm from 'components/forms/DocumentUploadForm.vue';
+import {Form} from 'src/helpers/form-helpers';
+import {ref, Ref} from 'vue';
+import {QForm} from 'quasar';
+import GenericForm from 'src/components/forms/GenericForm.vue'
+
+const emit = defineEmits(['submit'])
+
+const form_ref: Ref<QForm|null> = ref(null)
 
 const account_fields = [
   FIELDS.FILE_UPLOAD,
@@ -24,4 +35,21 @@ const pages = [
     fields: account_fields,
   },
 ]
+
+// Get copy of prop form
+const _pages = pages ? pages as Record<string, unknown>[] : undefined
+const form: Form = new Form(_pages)
+
+/**
+ * Validates and, if valid, submits the form with all entered values
+ * @async
+ */
+async function onSubmit(){
+  const is_valid = await form_ref.value?.validate()
+
+  if(is_valid){
+    emit('submit', form.values.value)
+  }
+
+}
 </script>
