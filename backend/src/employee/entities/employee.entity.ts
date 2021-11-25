b42dea40ffe72bddcd3d1f1998574c5787585e26
davwys <david.wyss@hotmail.ch>
2021-11-25 10:45:35 +0100

    Add person base entity

--- backend/src/employee/entities/employee.entity.ts
+++ backend/src/employee/entities/employee.entity.ts
@@ -1,8 +1,29 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { BaseEntity } from '../../base-entity/entities/base-entity.entity';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Person } from '../../person/entities/person.entity';
+import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
+import { Column } from 'typeorm';
 
 @ObjectType()
-export class Employee extends BaseEntity {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class Employee extends Person {
+  @Column()
+  @Field(() => String, { description: 'Language' })
+  @IsString()
+  language: string;
+
+  @Column()
+  @Field(() => String, { description: 'Function within the company' })
+  @IsString()
+  function: string;
+
+  @Column()
+  @Field(() => String, { description: 'E-mail' })
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @Field(() => String, { description: 'Phone Number' })
+  @IsString()
+  @IsPhoneNumber()
+  phone: string;
 }
