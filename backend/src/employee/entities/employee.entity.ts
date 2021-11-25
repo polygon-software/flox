00566cc01c760823d35ad2850d8afb7847b98070
davwys <david.wyss@hotmail.ch>
2021-11-25 10:59:20 +0100

    Employee setup2

--- backend/src/employee/entities/employee.entity.ts
+++ backend/src/employee/entities/employee.entity.ts
@@ -1,9 +1,11 @@
 import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
 import { Person } from '../../person/entities/person.entity';
 import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
-import { Column } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Company } from '../../company/entities/company.entity';
 
 @ObjectType()
+@Entity({ name: 'employee' })
 export class Employee extends Person {
   @Column()
   @Field(() => String, { description: 'Language' })
@@ -26,4 +28,14 @@ export class Employee extends Person {
   @IsString()
   @IsPhoneNumber()
   phone: string;
+
+  @Column()
+  @Field(() => String, { description: 'Gender' })
+  @IsString()
+  gender: string;
+
+  @Field(() => Company, { description: 'Company' })
+  @JoinColumn()
+  @OneToOne(() => Company)
+  company: Company;
 }
