09fa2465f64e4adc6a209b789737fef3854c2c58
davwys <david.wyss@hotmail.ch>
2021-12-03 09:35:15 +0100

    Picture adding setup

--- frontend/src/components/forms/fields/PictureUpload.vue
+++ frontend/src/components/forms/fields/PictureUpload.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- Image files -->
   <q-file
-    v-for="(field, index) in additionalInputFields"
+    v-for="(field, index) in pictures"
     :key="index"
     v-model="field.value"
     class="q-mb-md"
     outlined
     accept="image/*, .pdf"
-    :label="$t('documents.additional_documents')"
+    :label="$t('image') + ' ' + (index+1).toString()"
     stack-label
     clearable
     :max-file-size="props.maxFileSize"
@@ -31,60 +31,53 @@ const props = defineProps({
   }
 })
 
-const passport = ref(null)
-const commercialRegisterExtract = ref(null)
-const executionRegisterExtract = ref(null)
+const pictures: Ref<Array<Ref<null|File>>> = ref([ref(null)])
 
 /**
  * Emits the updated value
  */
 function emitValue(){
 
-  const validAdditionalInputFields = additionalInputFields.value.filter(field => {
+  const validPictures = pictures.value.filter(field => {
     return field.value !== null
   })
 
   // Add a name (e.g. 'additional_1') to every additional file
   const additionalFiles: Record<string, File> = {}
-  for(let i = 0; i < validAdditionalInputFields.length; i++){
+  for(let i = 0; i < validPictures.length; i++){
     const key = `additional_${i+1}`
-    const file = validAdditionalInputFields[i].value
+    const file = validPictures[i].value
     if(file){
       additionalFiles[key] = file
     }
   }
 
   // TODO inner validation?
-  emit('change', {passport, commercialRegisterExtract, executionRegisterExtract, ...additionalFiles})
+  emit('change', pictures)
 }
-
-/**
- * This section handles the addition and deletion of custom files.
- */
-const additionalInputFields: Ref<Array<Ref<null|File>>> = ref([ref(null)])
 
 /**
  * Depending on how many additional fields already exist, adds or deletes a file from a custom field.
  */
 function fileChange(): void {
-  const size = additionalInputFields.value.length
+  const size = pictures.value.length
 
   // Only 1 additional field
   if (size === 1) {
     // File was deleted -> do nothing
-    if (additionalInputFields.value[0].value === null) {
+    if (pictures.value[0].value === null) {
       emitValue()
       return;
     }
     // File was added -> add new field
-    additionalInputFields.value.push(ref(null))
+    pictures.value.push(ref(null))
     emitValue()
     return;
   }
 
   // Check if there's a field with model value === null
   for (let index=0; index<size; index++) {
-    const field = additionalInputFields.value[index]
+    const field = pictures.value[index]
     // A file was deleted
     if(field.value === null) {
       // Last field -> do nothing
@@ -93,13 +86,13 @@ function fileChange(): void {
         return;
       }
       // Not last element -> delete field
-      additionalInputFields.value.splice(index, 1)
+      pictures.value.splice(index, 1)
       emitValue()
       return;
     }
   }
   // File was added or updated
-  additionalInputFields.value.push(ref(null))
+  pictures.value.push(ref(null))
   emitValue()
 }
 
