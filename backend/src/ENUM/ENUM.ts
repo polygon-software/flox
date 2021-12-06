cbb318551c22693c76d003bc196827b1851e713c
davwys <david.wyss@hotmail.ch>
2021-12-06 13:45:07 +0100

    Enum stuff

--- backend/src/ENUM/ENUM.ts
+++ backend/src/ENUM/ENUM.ts
@@ -1,22 +1,9 @@
 import { registerEnumType } from '@nestjs/graphql';
-
-export enum CURRENCY {
-  CHF = 'chf',
-  EUR = 'eur',
-  USD = 'usd',
-}
-
-export enum CATEGORY {
-  TECHNOLOGY = 'technology',
-  // TODO others
-}
-
-export enum PRODUCT_STATUS {
-  DRAFT = 'draft',
-  ACTIVE = 'active',
-  ENDED = 'ended', // TODO proper definitions: what statuses are possible
-  WON = 'won',
-}
+import {
+  CATEGORY,
+  CURRENCY,
+  PRODUCT_STATUS,
+} from '../../../shared/definitions/ENUM';
 
 registerEnumType(CURRENCY, { name: 'Currency' });
 registerEnumType(CATEGORY, { name: 'Category' });
