ea2f6b4398a90498d03cb794b0e54703457bf94c
Joel Barmettler <joel.barmettler@uzh.ch>
2021-12-09 09:37:30 +0100

    Add sonarqube linter

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,10 +4,19 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+
+    // https://github.com/SonarSource/eslint-plugin-sonarjs
+    // linting rules that sonarqube considers for code quality checks
+    'sonarjs',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    // https://github.com/SonarSource/eslint-plugin-sonarjs
+    // linting rules that sonarqube considers for code quality checks
+    'plugin:sonarjs/recommended',
   ],
   root: true,
   env: {
