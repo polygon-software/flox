65cc2cceb56f37cf8e0ddbe053ef1b4ea914fc31
davwys <david.wyss@hotmail.ch>
2021-12-10 09:54:13 +0100

    Caching fix

--- backend/src/ENUM/ENUM.ts
+++ backend/src/ENUM/ENUM.ts
@@ -1,9 +1,23 @@
 import { registerEnumType } from '@nestjs/graphql';
-import {
-  CATEGORY,
-  CURRENCY,
-  PRODUCT_STATUS,
-} from '../../../shared/definitions/ENUM';
+
+export enum CURRENCY {
+  CHF = 'CHF',
+  EUR = 'EUR',
+  USD = 'USD',
+}
+
+export enum CATEGORY {
+  TECHNOLOGY = 'TECHNOLOGY',
+  // TODO others
+}
+
+export enum PRODUCT_STATUS {
+  DRAFT = 'DRAFT',
+  ACTIVE = 'ACTIVE',
+  ENDED = 'ENDED', // TODO proper definitions: what statuses are possible
+  WON = 'WON',
+  ARCHIVED = 'ARCHIVED',
+}
 
 registerEnumType(CURRENCY, { name: 'Currency' });
 registerEnumType(CATEGORY, { name: 'Category' });
