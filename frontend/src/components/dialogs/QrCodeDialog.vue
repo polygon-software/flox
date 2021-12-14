c7ec7d1a4967e1741bee135b4b27ca0f27b5872a
davwys <david.wyss@hotmail.ch>
2021-12-14 14:49:22 +0100

    Import fix

--- frontend/src/components/dialogs/QrCodeDialog.vue
+++ frontend/src/components/dialogs/QrCodeDialog.vue
@@ -35,7 +35,7 @@
 <script setup lang="ts">
 import QrcodeVue from 'qrcode.vue';
 import {defineEmits, defineProps, ref, Ref} from 'vue';
-import {QDialog, useDialogPluginComponent} from 'quasar'
+import {QDialog} from 'quasar'
 const dialog: Ref<QDialog|null> = ref<QDialog|null>(null)
 const emit = defineEmits(['ok'])
 // Mandatory - do not remove!
