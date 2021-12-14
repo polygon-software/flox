887e9854b1b8a21ed04067f6cb9b7685a437da5f
Johann Schwabe <johann.schwabe@uzh.ch>
2021-12-14 10:29:39 +0100

    merged

--- backend/src/modules/SOI-Admin/soi-admin.resolver.ts
+++ backend/src/modules/SOI-Admin/soi-admin.resolver.ts
@@ -5,23 +5,11 @@ import { CreateSoiAdminInput } from './dto/input/create-soi-admin.input';
 import { SoiAdminService } from './soi-admin.service';
 
 @Resolver(() => SoiAdmin)
-/**
- * A SOI Admin Resolver
- */
 export class SoiAdminResolver {
-  /**
-   * A constructor for the SOI Admin Resolver
-   * @param {SoiAdminService} soiAdminService - An SOI admin Service. Implicitly passed in
-   */
   constructor(private readonly soiAdminService: SoiAdminService) {}
 
   @AnyRole()
   @Mutation(() => SoiAdmin)
-  /**
-   * Create an SOI Admin
-   * @param {CreateSoiAdminInput} createSoiAdminInput - Field with with to create an SOI Admin
-   * @return {SoiAdmin} - The created SOI admin
-   */
   async createSoiAdmin(
     @Args('createSoiAdminInput') createSoiAdminInput: CreateSoiAdminInput,
   ): Promise<SoiAdmin> {
