94f44d36d88feebe8953ac42467a3250695f23d8
Elexa Heggli <elexa.heggli@gmx.ch>
2021-12-14 16:51:54 +0100

    added User

--- backend/src/modules/bank/bank.module.ts
+++ backend/src/modules/bank/bank.module.ts
@@ -4,9 +4,10 @@ import { Bank } from './entities/bank.entity';
 import { BankResolver } from './bank.resolver';
 import { BankService } from './bank.service';
 import { UserService } from '../user/user.service';
+import {User} from "../user/entities/user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bank])],
+  imports: [TypeOrmModule.forFeature([Bank, User])],
   providers: [BankResolver, BankService, UserService],
 })
 export class BankModule {}
