8fc6eb48cb70ecc9fe115103beeeab2890f49e17
davwys <david.wyss@hotmail.ch>
2021-12-16 09:50:06 +0100

    cleanup

--- backend/src/auth/authorization.decorator.ts
+++ backend/src/auth/authorization.decorator.ts
@@ -4,12 +4,12 @@ import {
   SetMetadata,
 } from '@nestjs/common';
 import { getRequest } from '../helpers';
+import { ROLE } from '../ENUM/ENUM';
 
 /**
  * Defines authorization-specific decorators
  */
 
-export const IS_MINE_KEY = 'isMine';
 export const ANY_ROLE_KEY = 'anyRole';
 
 // Restrict to specified roles
@@ -17,15 +17,11 @@ export const Roles = (...roles: string[]): CustomDecorator =>
   SetMetadata('roles', roles);
 
 // Restrict to admin role
-export const AdminOnly = (): CustomDecorator => SetMetadata('roles', ['admin']);
+export const AdminOnly = (): CustomDecorator =>
+  SetMetadata('roles', [ROLE.ADMIN]);
 
 // Allows access with any role
 export const AnyRole = (): CustomDecorator => SetMetadata(ANY_ROLE_KEY, true);
-
-// TODO: Application-specific role decorators go here
-
-// Restrict to resources owned by me (where 'owner' is the user's UUID)
-export const Mine = (): CustomDecorator => SetMetadata(IS_MINE_KEY, true);
 
 // Access to current user from request
 export const CurrentUser = createParamDecorator(
