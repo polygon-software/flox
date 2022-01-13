c093cb9ea7c90e67274af8ee5780b5e914172ca9
thommann <thomasrolf.mannhart@uzh.ch>
2022-01-13 13:09:57 +0100

    Announcement creation and broadcasting

--- backend/src/modules/announcement/announcement.module.ts
+++ backend/src/modules/announcement/announcement.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Announcement } from './entities/announcement.entity';
 import { AnnouncementResolver } from './announcement.resolver';
 import { AnnouncementService } from './announcement.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Announcement])],
+  imports: [TypeOrmModule.forFeature([Announcement]), UserModule],
   providers: [AnnouncementResolver, AnnouncementService],
 })
 export class AnnouncementModule {}
--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -4,9 +4,11 @@ import { UserResolver } from './user.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Address } from '../address/entities/address.entity';
+import { Notification } from '../notification/entities/notification.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Address])],
+  imports: [TypeOrmModule.forFeature([User, Address, Notification])],
   providers: [UserResolver, UserService],
+  exports: [UserService],
 })
 export class UserModule {}
