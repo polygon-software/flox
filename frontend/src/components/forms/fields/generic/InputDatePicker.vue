a2ae264efa6916bad565481086fab4c3d3afd580
Elexa Heggli <elexa.heggli@gmx.ch>
2022-01-17 13:52:59 +0100

    added Warning Dialog

--- frontend/src/components/forms/fields/generic/InputDatePicker.vue
+++ frontend/src/components/forms/fields/generic/InputDatePicker.vue
@@ -1,16 +1,18 @@
 <template>
   <div >
-    <p>Test:  {{props.dateLabel}}</p>
     <q-input
       v-model="date"
       mask="date"
       :rules="['date']"
       dense
-      :label="props.dateLabel">
+      :label="label">
       <template #append>
         <q-icon name="event" class="cursor-pointer">
           <q-popup-proxy ref="qDateProxy" cover transition-show="scale" transition-hide="scale">
-            <q-date v-model="date" @change="emitValue">
+            <q-date
+              v-model="date"
+              @update:model-value="dateInput"
+              @change="emitValue">
               <div class="row items-center justify-end">
                 <q-btn v-close-popup label="Close" color="primary" flat />
               </div>
@@ -23,15 +25,18 @@
 </template>
 
 <script setup lang="ts">
-import {defineProps, ref} from 'vue'
+import {defineProps, ref} from 'vue';
+import {i18n} from 'boot/i18n';
+import {useQuasar} from "quasar";
 
 const props = defineProps({
-  dateLabel: {
+  label: {
     type: String,
     required: true,
   },
 })
 const emit = defineEmits(['change'])
+const $q = useQuasar()
 
 const date = ref(new Date())
 
@@ -41,5 +46,15 @@ const date = ref(new Date())
  */
 function emitValue(){
   emit('change', date)
+}
+
+function dateInput(date: Date){
+  const date60yearsAgo = new Date(new Date().setFullYear(new Date().getFullYear() - 60))
+  if(date < date60yearsAgo){
+    $q.dialog({
+      component: WarningDialog,
+      componentProps: {description: i18n.global.t('form_for_clients.retirement_warning')}
+    })
+  }
 }
 </script>
