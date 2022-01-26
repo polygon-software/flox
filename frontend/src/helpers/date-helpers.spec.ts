d2a57274201e3eb4871d17723acd066e6dbf5cb5
ramizee <43523869+ramizee@users.noreply.github.com>
2022-01-26 09:07:06 +0100

    date helper test

---
+++ frontend/src/helpers/date-helpers.spec.ts
@@ -0,0 +1,22 @@
+import {calculateAge, dateToInputString} from "src/helpers/date-helpers";
+
+describe('Date Helpers', () => {
+  describe('calculated age', () => {
+    let birthday: Date
+    beforeEach( () => {
+      birthday = new Date('2014-01-31T00:00:00.000Z')
+    })
+    it('should return the age given his/her birthday as number', () => {
+      expect(calculateAge(birthday)).toBeLessThanOrEqual(7)
+      expect(calculateAge(birthday)).toBeGreaterThanOrEqual(0)
+      expect(calculateAge(birthday)).not.toBeLessThanOrEqual(3)
+      expect(calculateAge(birthday)).toEqual(expect.any(Number))
+    });
+
+    it('should return a date as an string', () => {
+      expect(dateToInputString(birthday)).toStrictEqual('2014-01-31')
+      expect(dateToInputString(birthday)).not.toStrictEqual('2013-01-31')
+      expect(dateToInputString(birthday)).toEqual(expect.any(String))
+    });
+  });
+});
