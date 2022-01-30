ee26531c98d0607a99d2e9beb4bd4fff6db27efc
ramizee <43523869+ramizee@users.noreply.github.com>
2022-01-30 13:30:52 +0100

    general helpers finished

--- frontend/src/helpers/general-helpers.spec.ts
+++ frontend/src/helpers/general-helpers.spec.ts
@@ -3,7 +3,11 @@ import {sleep} from "src/helpers/general-helpers";
 describe('General Helpers', () => {
   describe('time to wait', () => {
     it('should return the time in ms to wait', () => {
-      expect(sleep()).toBeLessThanOrEqual(100)
+      const time_before_sleep_function = new Date()
+      sleep()
+      const time_after_sleep_function = new Date()
+      const difference_in_milliseconds = time_after_sleep_function.getMilliseconds() - time_before_sleep_function.getMilliseconds()
+      expect(difference_in_milliseconds).toBeLessThanOrEqual(100)
     });
   });
 });
