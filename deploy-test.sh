a80e4de18f4781962f4d52db1b7396feb0fbce94
davwys <david.wyss@hotmail.ch>
2022-01-31 20:27:49 +0100

    Add deploy script

---
+++ deploy-test.sh
@@ -0,0 +1,22 @@
+# PREREQUISITE: An ECS context, named 'soicontext' with valid credentials
+
+docker context use default
+
+# Build & push frontend
+echo Updating frontend...
+cd frontend || return
+docker build -t polygonsoftware/soi-frontend .
+docker push polygonsoftware/soi-frontend
+
+# Build & push backend
+echo Updating backend...
+cd ..
+cd backend || return
+docker build -t polygonsoftware/soi .
+docker push polygonsoftware/soi
+
+# Upload new versions
+echo Deploying new versions to AWS...
+cd ..
+docker context use soicontext
+docker compose up
