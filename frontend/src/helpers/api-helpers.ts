e0127f0c84d0693772ca78cf60639ca430f3db1d
thommann <thomasrolf.mannhart@uzh.ch>
2022-02-01 10:15:21 +0100

    fetch my products

--- frontend/src/helpers/api-helpers.ts
+++ frontend/src/helpers/api-helpers.ts
@@ -3,7 +3,7 @@ import { subscribeToQuery } from 'src/helpers/data-helpers';
 import { MY_NOTIFICATIONS } from 'src/data/queries/NOTIFICATIONS';
 import { computed, Ref } from 'vue';
 import { Product } from 'src/data/types/Product';
-import { ALL_PRODUCTS } from 'src/data/queries/PRODUCT';
+import { ALL_PRODUCTS, MY_PRODUCTS } from 'src/data/queries/PRODUCT';
 import {
   CATEGORY,
   CURRENCY,
@@ -21,20 +21,7 @@ export function fetchMyNotifications(): Ref<Notification[]> {
   const queryResult = subscribeToQuery(MY_NOTIFICATIONS) as Ref<
     Record<string, unknown>[]
   >;
-
-  return computed(() => {
-    const records = queryResult.value ?? [];
-    return records.map(
-      (record) =>
-        new Notification(
-          record.title as string,
-          new Date(record.received as string),
-          record.content as string,
-          record.isRead as boolean,
-          record.uuid as string
-        )
-    );
-  });
+  return computed(() => mapNotifications(queryResult));
 }
 
 /**
@@ -45,42 +32,18 @@ export function fetchAllProducts(): Ref<Product[]> {
   const queryResult = subscribeToQuery(ALL_PRODUCTS) as Ref<
     Record<string, unknown>[]
   >;
+  return computed(() => mapProducts(queryResult));
+}
 
-  return computed(() => {
-    const productRecords = queryResult?.value ?? [];
-    return productRecords.map(
-      (record) =>
-        new Product(
-          record.uuid as string,
-          record.title as string,
-          record.description as string,
-          record.brand as string,
-          record.category as CATEGORY,
-          record.value as number,
-          record.currency as CURRENCY,
-          new Date(record.start as string),
-          new Date(record.end as string),
-          record.pictures as Record<string, string>[],
-          record.status as PRODUCT_STATUS,
-          record.sponsored as boolean,
-          record.directBuyLink as string,
-          record.directBuyLinkCLicks as number,
-          record.directBuyLinkMaxClicks as number,
-          record.directBuyLinkCost as number,
-          record.directBuyLinkMaxCost as number,
-          record.brandLink as string,
-          record.brandLinkClicks as number,
-          record.brandLinkMaxClicks as number,
-          record.brandLinkCost as number,
-          record.brandLinkMaxCost as number,
-          record.minBet as number,
-          record.maxBet as number,
-          record.tags as string[],
-          record.comments as Record<string, string>[],
-          record.likes as number
-        )
-    );
-  });
+/**
+ * Fetch my products.
+ * @returns {Ref<Product[]>} - my products
+ */
+export function fetchMyProducts(): Ref<Product[]> {
+  const queryResult = subscribeToQuery(MY_PRODUCTS) as Ref<
+    Record<string, unknown>[]
+  >;
+  return computed(() => mapProducts(queryResult));
 }
 
 /**
@@ -91,19 +54,91 @@ export function fetchAllAnnouncements(): Ref<Announcement[]> {
   const queryResult = subscribeToQuery(ALL_ANNOUNCEMENTS) as Ref<
     Record<string, unknown>[]
   >;
+  return computed(() => mapAnnouncements(queryResult));
+}
 
-  return computed(() => {
-    const records = queryResult.value ?? [];
-    return records.map(
-      (record) =>
-        new Announcement(
-          record.title as string,
-          new Date(record.date as string),
-          record.content as string,
-          record.userRoles as ROLE[],
-          record.scheduled as boolean,
-          record.uuid as string
-        )
-    );
-  });
+/**
+ * Map notification records to notification instances.
+ * @param {Ref<Record<string, unknown>[]>} queryResult - notification records.
+ * @returns {Notification[]} - notification instances.
+ */
+export function mapNotifications(
+  queryResult: Ref<Record<string, unknown>[]>
+): Notification[] {
+  const records = queryResult.value ?? [];
+  return records.map(
+    (record) =>
+      new Notification(
+        record.title as string,
+        new Date(record.received as string),
+        record.content as string,
+        record.isRead as boolean,
+        record.uuid as string
+      )
+  );
+}
+
+/**
+ * Map product records to product instances.
+ * @param {Ref<Record<string, unknown>[]>} queryResult - product records.
+ * @returns {Product[]} - product instances.
+ */
+export function mapProducts(
+  queryResult: Ref<Record<string, unknown>[]>
+): Product[] {
+  const productRecords = queryResult.value ?? [];
+  return productRecords.map(
+    (record) =>
+      new Product(
+        record.uuid as string,
+        record.title as string,
+        record.description as string,
+        record.brand as string,
+        record.category as CATEGORY,
+        record.value as number,
+        record.currency as CURRENCY,
+        new Date(record.start as string),
+        new Date(record.end as string),
+        record.pictures as Record<string, string>[],
+        record.status as PRODUCT_STATUS,
+        record.sponsored as boolean,
+        record.directBuyLink as string,
+        record.directBuyLinkCLicks as number,
+        record.directBuyLinkMaxClicks as number,
+        record.directBuyLinkCost as number,
+        record.directBuyLinkMaxCost as number,
+        record.brandLink as string,
+        record.brandLinkClicks as number,
+        record.brandLinkMaxClicks as number,
+        record.brandLinkCost as number,
+        record.brandLinkMaxCost as number,
+        record.minBet as number,
+        record.maxBet as number,
+        record.tags as string[],
+        record.comments as Record<string, string>[],
+        record.likes as number
+      )
+  );
+}
+
+/**
+ * Map announcement records to announcement instances.
+ * @param {Ref<Record<string, unknown>[]>} queryResult - announcement records.
+ * @returns {Announcement[]} - announcement instances.
+ */
+export function mapAnnouncements(
+  queryResult: Ref<Record<string, unknown>[]>
+): Announcement[] {
+  const records = queryResult.value ?? [];
+  return records.map(
+    (record) =>
+      new Announcement(
+        record.title as string,
+        new Date(record.date as string),
+        record.content as string,
+        record.userRoles as ROLE[],
+        record.scheduled as boolean,
+        record.uuid as string
+      )
+  );
 }
