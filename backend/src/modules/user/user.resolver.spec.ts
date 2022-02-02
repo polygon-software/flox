0f01d4629b2f6e7479312550575b188ddf45e2d7
thommann <thomasrolf.mannhart@uzh.ch>
2022-02-02 11:10:16 +0100

    tests :(

--- backend/src/modules/user/user.resolver.spec.ts
+++ backend/src/modules/user/user.resolver.spec.ts
@@ -2,15 +2,21 @@ import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { NotificationService } from '../notification/notification.service';
+import { Notification } from '../notification/entities/notification.entity';
 
 describe('UsersResolver', () => {
   let userResolver: UserResolver;
   let userService: UserService;
   let userRepository: Repository<User>;
+  let notificationService: NotificationService;
+  let notificationRepository: Repository<Notification>;
 
   beforeEach(async () => {
     userRepository = new Repository<User>();
-    userService = new UserService(userRepository);
+    notificationRepository = new Repository<Notification>();
+    notificationService = new NotificationService(notificationRepository);
+    userService = new UserService(userRepository, notificationService);
     userResolver = new UserResolver(userService);
   });
 
