04eb89aa76ebc5b59df9535be8c16def7ee25df1
davwys <david.wyss@hotmail.ch>
2022-02-18 08:32:13 +0100

    Remove old test content

--- backend/src/modules/user/user.service.spec.ts
+++ backend/src/modules/user/user.service.spec.ts
@@ -1,28 +1,14 @@
 import { UserService } from './user.service';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { NotificationService } from '../notification/notification.service';
-import { Notification } from '../notification/entities/notification.entity';
-import { Announcement } from '../announcement/entities/announcement.entity';
 
 describe('UsersService', () => {
   let userService: UserService;
   let userRepository: Repository<User>;
-  let notificationService: NotificationService;
-  let notificationRepository: Repository<Notification>;
-  let announcementRepository: Repository<Announcement>;
 
   beforeEach(async () => {
     userRepository = new Repository<User>();
-    notificationRepository = new Repository<Notification>();
-    announcementRepository = new Repository<Announcement>();
-
-    notificationService = new NotificationService(
-      notificationRepository,
-      userRepository,
-      announcementRepository,
-    );
-    userService = new UserService(userRepository, notificationService);
+    userService = new UserService(userRepository);
   });
 
   it('should be defined', () => {
