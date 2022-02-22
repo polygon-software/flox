b2e81397e0ccbbf43a790c02aa6d11359975d1ff
davwys <david.wyss@hotmail.ch>
2022-02-22 14:44:20 +0100

    Add comment

--- backend/src/types/Device.ts
+++ backend/src/types/Device.ts
@@ -2,6 +2,10 @@ import { createUnionType } from '@nestjs/graphql';
 import { MR2000 } from './MR2000';
 import { MR3000 } from './MR3000';
 
+/**
+ * This type is a Wrapper for the union of MR2000 and MR3000 devices, to be used with TypeORM
+ */
+
 export const Device = createUnionType({
   name: 'Device',
   types: () => [MR2000, MR3000],
