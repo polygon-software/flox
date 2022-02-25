c36303d23b98229d4864da28c948f165b998186a
johannschwabe <34106718+johannschwabe@users.noreply.github.com>
2022-02-25 13:09:40 +0100

    feat(soi): Banks only see dossiers with all required files (#283)
    * Fix routing stuff
    
    * Success page fixes
    
    * Add correct success messages / redirects
    
    * Fix cache login issue, still throwing errors currently
    
    * Fix errors
    
    * Minor fix on bank dashboard
    
    * Accept only PDF in offer upload
    
    * Add loading indicator to value estimate
    
    * multi-submit prevention
    
    * Various minor import fixes
    
    * Fix SSR with CompanyEmployeeIdCard
    
    * fixed cache
    
    * added minimal implementation
    
    * removed unused imports
    
    * fixed option/required in frontend
    
    * fixed typos
    
    Co-authored-by: davwys <david.wyss@hotmail.ch>

---
+++ backend/src/modules/dossier/dossier-helpers.ts
@@ -0,0 +1,56 @@
+import { Dossier } from './entity/dossier.entity';
+import { DOSSIER_FILE_TYPE, PROPERTY_TYPE } from '../../ENUM/ENUMS';
+
+const needed = [
+  DOSSIER_FILE_TYPE.ID,
+  DOSSIER_FILE_TYPE.SALARY,
+  DOSSIER_FILE_TYPE.PENSION,
+  DOSSIER_FILE_TYPE.LAST_YEAR_SALARY,
+  DOSSIER_FILE_TYPE.PENSION_ID,
+  DOSSIER_FILE_TYPE.MORTGAGE_CONTRACT,
+  DOSSIER_FILE_TYPE.PRODUCT_AGREEMENT,
+  DOSSIER_FILE_TYPE.FLOOR_PLANS,
+  DOSSIER_FILE_TYPE.PICTURES,
+  DOSSIER_FILE_TYPE.PURCHASE_CONTRACT,
+];
+
+/**
+ * Checks whether all required files are present
+ * @param {Dossier} dossier - the dossier
+ * @returns {boolean} - complete
+ */
+export function isCompleted(dossier: Dossier): boolean {
+  const complete = needed.every((type) => {
+    return !!dossier.documents.find((docu) => {
+      return (docu.file_type as unknown as DOSSIER_FILE_TYPE) == type;
+    });
+  });
+  const owner_regulations =
+    dossier.property_type !== PROPERTY_TYPE.APARTMENT ||
+    !!dossier.documents.find(
+      (docu) =>
+        (docu.file_type as unknown as DOSSIER_FILE_TYPE) ==
+        DOSSIER_FILE_TYPE.OWNER_REGULATIONS,
+    );
+  const management_regulations =
+    dossier.property_type !== PROPERTY_TYPE.APARTMENT ||
+    !!dossier.documents.find(
+      (docu) =>
+        (docu.file_type as unknown as DOSSIER_FILE_TYPE) ==
+        DOSSIER_FILE_TYPE.MANAGEMENT_REGULATIONS,
+    );
+
+  const building_insurance =
+    dossier.property_type !== PROPERTY_TYPE.ONE_FAMILY_HOUSE ||
+    !!dossier.documents.find(
+      (docu) =>
+        (docu.file_type as unknown as DOSSIER_FILE_TYPE) ==
+        DOSSIER_FILE_TYPE.BUILDING_INSURANCE,
+    );
+  return (
+    complete &&
+    owner_regulations &&
+    management_regulations &&
+    building_insurance
+  );
+}
