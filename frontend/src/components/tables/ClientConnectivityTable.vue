5f5585aa8e295575cad08033090687121b7162f7
davwys <david.wyss@hotmail.ch>
2022-03-17 08:58:22 +0100

    Serverside pagination

--- frontend/src/components/tables/ClientConnectivityTable.vue
+++ frontend/src/components/tables/ClientConnectivityTable.vue
@@ -9,6 +9,7 @@
         outlined
         type="search"
         dense
+        debounce="300"
       >
         <template #append>
           <q-icon name="search" />
@@ -16,15 +17,18 @@
       </q-input>
     </div>
     <q-table
+      v-model:pagination="pagination"
       class="q-mt-lg"
-      flat
       :rows="rows"
+      flat
       :columns="columns"
-      row-key="uuid"
+      :loading="loading"
+      row-key="id"
       :filter="search"
       :filter-method="tableFilter"
-      :rows-per-page-options="[10,20, 100]"
+      :rows-per-page-options="[10]"
       separator="none"
+      @request="onRequest"
     >
       <template #body="_props">
         <q-tr
@@ -52,13 +56,13 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, Ref, ref, defineProps} from 'vue';
+import {onMounted, Ref, ref, defineProps, reactive} from 'vue';
 import {tableFilter} from 'src/helpers/filter-helpers';
 import {i18n} from 'boot/i18n';
 import {ConnectionLogEntry} from 'src/data/types/ConnectionLogEntry';
 import {connectionLogForDevice} from 'src/helpers/api-helpers';
 import {formatDateTime} from 'src/helpers/format-helpers';
-import {DEVICE_CONNECTION_LOG_COUNT, DEVICE_CONNECTION_LOGS} from 'src/data/queries/DEVICE';
+import {DEVICE_CONNECTION_LOG_COUNT} from 'src/data/queries/DEVICE';
 import {executeQuery} from 'src/helpers/data-helpers';
 
 const props = defineProps({
@@ -73,14 +77,25 @@ const search = ref('')
 // Pagination
 const skip = ref(0)
 const take = ref(10) // Load first 10 entries by default
-const totalRows: Ref<number|null> = ref(null)
+const loading = ref(true)
+
+// QTable pagination
+const pagination = ref(
+  {
+    sortBy: 'timestamp',
+    descending: false,
+    page: 1,
+    rowsNumber: 1, // initially assume at least 1 entry
+    rowsPerPage: take.value
+  }
+)
 
 // ----- Data -----
 const columns = [
-  { name: 'timestamp', label: i18n.global.t('client_connectivity.date_time'), field: 'timestamp', sortable: true, align: 'center' },
-  { name: 'realIp', label: i18n.global.t('client_connectivity.real_ip'), field: 'realIp', sortable: true, align: 'center' },
-  { name: 'port', label: i18n.global.t('client_connectivity.port'), field: 'port', sortable: true, align: 'center' },
-  { name: 'vpnIp', label: i18n.global.t('client_connectivity.vpn_ip'), field: 'vpnIp', sortable: true, align: 'center' },
+  { name: 'timestamp', label: i18n.global.t('client_connectivity.date_time'), field: 'timestamp', sortable: false, align: 'center' },
+  { name: 'realIp', label: i18n.global.t('client_connectivity.real_ip'), field: 'realIp', sortable: false, align: 'center' },
+  { name: 'port', label: i18n.global.t('client_connectivity.port'), field: 'port', sortable: false, align: 'center' },
+  { name: 'vpnIp', label: i18n.global.t('client_connectivity.vpn_ip'), field: 'vpnIp', sortable: false, align: 'center' },
   { name: 'event', label: i18n.global.t('client_connectivity.event'), field: 'event', sortable: false, align: 'center' },
 ]
 
@@ -88,12 +103,38 @@ const rows: Ref<ConnectionLogEntry[]> = ref([])
 
 // Fetch logs on mount
 onMounted(async () => {
-  await fetchLogs()
-
   // Get total row count (for pagination)
   const countQueryResult = await executeQuery(DEVICE_CONNECTION_LOG_COUNT, {cli: props.cli})
-  totalRows.value = countQueryResult.data[DEVICE_CONNECTION_LOG_COUNT.cacheLocation] as number
+  pagination.value.rowsNumber = countQueryResult.data[DEVICE_CONNECTION_LOG_COUNT.cacheLocation] as number
+
+  // Fetch initial data
+  await onRequest({
+    pagination: pagination.value,
+  })
 })
+
+/**
+ * Handle server-side pagination on new page request
+ * @param {Record<string, Record<string, number>>} reqProps - new pagination data
+ * @returns {Promise<void>} - done
+ */
+async function onRequest(reqProps: Record<string, Record<string, number|string|boolean>>){
+  loading.value = true
+
+  console.log('onrequest', reqProps)
+  const { page, rowsPerPage } = reqProps.pagination
+
+  // Update pagination
+  pagination.value.page = page as number
+  pagination.value.rowsPerPage = rowsPerPage as number
+
+  // Update fetching properties
+  take.value = rowsPerPage as number
+  skip.value = ((page as number)-1) * take.value
+
+  // Re-fetch
+  await fetchLogs()
+}
 
 /**
  * Fetches table contents
@@ -101,10 +142,7 @@ onMounted(async () => {
  */
 async function fetchLogs(){
   rows.value = await connectionLogForDevice(props.cli, skip.value, take.value)
+  loading.value = false
 }
 
 </script>
-
-<style scoped>
-
-</style>
