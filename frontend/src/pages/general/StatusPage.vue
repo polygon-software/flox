a3f81d9c3d204ef125167d49ae5a76238f4605a1
davwys <david.wyss@hotmail.ch>
2022-03-21 16:42:41 +0100

    Type-dependent tables

--- frontend/src/pages/general/StatusPage.vue
+++ frontend/src/pages/general/StatusPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="column items-center justify-start full-width">
+  <q-page class="column items-center justify-start full-width q-pb-xl">
     <!-- Title -->
     <h5>{{ $t('status.title') }} {{ stationId }}</h5>
 
@@ -7,18 +7,25 @@
 
     <LogFilesTable :cli="stationId"/>
 
-    <FTPLogFilesTable :cli="stationId"/>
+    <FTPLogFilesTable
+      v-if="deviceType === 'MR3000'"
+      :cli="stationId"
+    />
 
-    <RESTLogFilesTable :cli="stationId"/>
+    <RESTLogFilesTable
+      v-if="deviceType === 'MR3000'"
+      :cli="stationId"
+    />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import ClientConnectivityTable from 'components/tables/ClientConnectivityTable.vue';
 import LogFilesTable from 'components/tables/DeviceLogTable.vue';
-import {defineProps} from 'vue';
+import {computed, defineProps} from 'vue';
 import FTPLogFilesTable from 'components/tables/FTPLogFilesTable.vue';
 import RESTLogFilesTable from 'components/tables/RESTLogFilesTable.vue';
+import {deviceType} from 'src/helpers/device-helpers';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = defineProps({
@@ -26,6 +33,12 @@ const props = defineProps({
     required: true,
     type: String
   }
+})
+
+// Device type (MR2000 or MR3000)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const type = computed(() => {
+  return deviceType(props.stationId)
 })
 
 </script>
