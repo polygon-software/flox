f656f87aaa49e682d8b814ace2716613f0d65f3f
davwys <david.wyss@hotmail.ch>
2022-05-27 07:52:16 +0200

    Add flox config

---
+++ backend/src/flox.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Flox modules to enable
+  modules: {
+    auth: true,
+    roles: true,
+    file: true,
+  },
+  moduleOptions: {
+    roles: {
+      // Some options could go here
+    },
+  },
+};
