12b91d847d605a70208b3f2da28f6a85f31cf4c4
davwys <david.wyss@hotmail.ch>
2022-05-27 07:51:35 +0200

    Setup flox helpers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 import { UserModule } from './flox-modules/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import configuration from './flox-modules/config/configuration';
+import configuration from './config/configuration';
 import { Context } from 'vm';
 import { JwtAuthGuard } from './flox-modules/auth/auth.guard';
 import { APP_GUARD } from '@nestjs/core';
@@ -18,14 +18,11 @@ import { RolesGuard } from './flox-modules/auth/roles.guard';
   imports: [
     GraphQLModule.forRoot({
       playground: true,
-      //services: [ApolloServerPluginLandingPageLocalDefault()], // Use Apollo Sandbox instead of graphql-playground
       debug: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
-      //disableHealthCheck: true //set true if using multiple GraphQL endpoints in a single application with fastify
       installSubscriptionHandlers: true,
       subscriptions: {
-        // Could also use graphql-ws instead of default (subscriptions-transport-ws)
         'subscriptions-transport-ws': {
           path: '/graphql-websocket',
           onConnect: (context: Context) => {
