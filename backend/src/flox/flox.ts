e49ca9f8f6a296a74512423e170e85552626577e
davwys <david.wyss@hotmail.ch>
2022-05-30 10:17:50 +0200

    Helpers

--- backend/src/flox/flox.ts
+++ backend/src/flox/flox.ts
@@ -88,6 +88,35 @@ function getActiveFloxModuleNames() {
 }
 
 /**
+ * Gets the active Flox modules' options (with proper typing, since config is .js)
+ * @returns {Record<string, Record<string, unknown>>} - options for active modules
+ */
+export function floxModulesOptions() {
+  const options: Record<string, Record<string, unknown>> = {};
+
+  // Get active modules
+  const modules = floxModules();
+
+  modules.forEach((module) => {
+    options[module] = (flox.moduleOptions[module] ?? {}) as Record<
+      string,
+      unknown
+    >;
+  });
+
+  return options;
+}
+
+/**
+ * Gets the options for a single Flox module (with proper typing, since config is .js)
+ * @param {string} moduleName - name of the module to check
+ * @returns {Record<string, Record<string, unknown>>} - options for the modules
+ */
+export function floxModuleOptions(moduleName: string) {
+  return (flox.moduleOptions[moduleName] ?? {}) as Record<string, unknown>;
+}
+
+/**
  * Determines whether a Flox module is currently active from flox.config.js
  * @param {string} moduleName - name of the module to check
  * @returns {boolean} - whether the module is active
