f167195fcd90435d3f47a1fce24792d044ac4b1d
davwys <david.wyss@hotmail.ch>
2022-06-01 13:51:47 +0200

    Improved testing setup, still has issues

--- backend/src/flox/modules/auth/user.resolver.spec.ts
+++ backend/src/flox/modules/auth/user.resolver.spec.ts
@@ -2,6 +2,8 @@ import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { DEFAULT_ROLES } from '../roles';
 
 describe('UsersResolver', () => {
   let userResolver: UserResolver;
@@ -17,4 +19,28 @@ describe('UsersResolver', () => {
   it('should be defined', () => {
     expect(userResolver).toBeDefined();
   });
+
+  it('should create a user', async () => {
+    // No users present at start
+    let numberOfUsers = await userRepository.count();
+    expect(numberOfUsers).toBe(0);
+
+    const input: CreateUserInput = {
+      username: 'Test User',
+      email: 'test@test.com',
+      cognitoUuid: '1234-abcd-4567',
+      role: DEFAULT_ROLES.ADMIN,
+    };
+
+    // Create user
+    await userResolver.createUser(input);
+
+    // Ensure user was created
+    numberOfUsers = await userRepository.count();
+    expect(numberOfUsers).toBe(1);
+  });
+
+  // it('should throw an error when creating a user with a disallowed role', () => {
+  //   expect(userResolver).toBeDefined();
+  // });
 });
