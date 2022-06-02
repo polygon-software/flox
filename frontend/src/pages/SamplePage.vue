c97b6c1871b5f419248f0c59fdd3636c5ed23d69
davwys <david.wyss@hotmail.ch>
2022-06-02 11:34:47 +0200

    remove sample form

--- frontend/src/pages/SamplePage.vue
+++ frontend/src/pages/SamplePage.vue
@@ -1,17 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-
     <!-- Active Flox modules -->
     <ModuleStatus/>
-
-    <!-- Test table for visualizing data fetched via GraphQL/Apollo -->
-    <div class="column">
-      <SampleForm/>
-    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import SampleForm from 'components/sample/SampleForm.vue'
 import ModuleStatus from 'components/sample/ModuleStatus.vue';
 </script>
