caaeacc3924f1671cb6f241ad686f3f649b4a149
davwys <david.wyss@hotmail.ch>
2022-06-23 09:56:41 +0200

    User pool changes

--- scripts/cognito-tf/cognito.tf
+++ scripts/cognito-tf/cognito.tf
@@ -1,9 +1,10 @@
-# TODO: do based on flox.config.js
+# Cognito user pool setup
+
 resource "aws_cognito_user_pool" "user_pool" {
   name = var.project
-  auto_verified_attributes   = ["email"]
-  username_attributes        = ["email"]
-  mfa_configuration = "ON"
+  auto_verified_attributes   = var.auto_verified_attributes
+  username_attributes        = var.username_attributes
+  mfa_configuration = var.mfa_configuration
   software_token_mfa_configuration {
     enabled = true
   }
@@ -13,7 +14,7 @@ resource "aws_cognito_user_pool" "user_pool" {
 }
 
 resource "aws_cognito_user_pool_client" "app_client" {
-  name         = "${var.project}Client" # TODO: based on .env
+  name         = "${var.project}Client"
   user_pool_id = aws_cognito_user_pool.user_pool.id
   access_token_validity = 1
   token_validity_units {
