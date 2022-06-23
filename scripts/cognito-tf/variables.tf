a8850661f0753fcee66be9b7e65891ff76a54a10
davwys <david.wyss@hotmail.ch>
2022-06-23 09:56:41 +0200

    User pool changes

--- scripts/cognito-tf/variables.tf
+++ scripts/cognito-tf/variables.tf
@@ -1,5 +1,25 @@
+# Cognito configuration TODO: adapt based on flox.config
+
 variable "project" {
   default         = "flox" # TODO: set based on .env?
   description     = "prefix used for all resources in the Project"
   type            = string
+}
+
+variable "mfa_configuration" {
+  default         = "ON"
+  description     = "Cognito MFA mode"
+  type            = string
+}
+
+variable "auto_verified_attributes"{
+  default         = ["email"]
+  description     = "Cognito attributes to auto-verify"
+  type            = set(string)
+}
+
+variable "username_attributes"{
+  default         = ["email"]
+  description     = "Cognito username attributes"
+  type            = set(string)
 }
