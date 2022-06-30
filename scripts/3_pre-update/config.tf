7e1c7b9b19677fb28b0bae4d67873de499eeb474
davwys <david.wyss@hotmail.ch>
2022-06-30 16:47:37 +0200

    Script fixes

--- scripts/3_pre-update/config.tf
+++ scripts/3_pre-update/config.tf
@@ -7,10 +7,10 @@ terraform {
   }
   required_version      = ">= 0.14.9"
   cloud {
-    organization        = "##ORGANISATION##"
+    organization        = "polygonsoftware"
 
     workspaces {
-      name = "##PROJECT##-##TYPE##" # will be replaced in preprocessing
+      name = "flox-test-pre-update" # will be replaced in preprocessing
     }
   }
 }
