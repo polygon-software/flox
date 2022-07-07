cbf079d67824ef7abd2c16638d3d1c95289ec9a5
davwys <david.wyss@hotmail.ch>
2022-07-07 17:13:01 +0200

    Clean S3 deploy

--- scripts/2_main-setup/web-spa-pwa/iam.tf
+++ scripts/2_main-setup/web-spa-pwa/iam.tf
@@ -5,7 +5,9 @@ data "aws_iam_policy_document" "website_bucket_policy" {
 
     principals {
       type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+      identifiers = ["*"]
     }
   }
 }
+
+#TODO aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
