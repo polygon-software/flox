319fda826e4db4476b9afd1a93b58203a8d6d641
davwys <david.wyss@hotmail.ch>
2022-07-12 11:16:07 +0200

    Fix S3 redirect (PWA mode)

--- scripts/2_main-setup/web-spa-pwa/dns.tf
+++ scripts/2_main-setup/web-spa-pwa/dns.tf
@@ -27,3 +27,14 @@ resource "aws_acm_certificate_validation" "cert_validation_frontend" {
   certificate_arn         = aws_acm_certificate.frontend_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.ssl_frontend : record.fqdn]
 }
+
+resource "aws_route53_record" "redirect_record" {
+  name                  = "www.${var.domain}"
+  type                  = "A"
+  zone_id               = var.hosted_zone_id
+  alias {
+    evaluate_target_health = false
+    name                   = aws_s3_bucket.redirect_bucket.website_domain
+    zone_id                = aws_s3_bucket.redirect_bucket.hosted_zone_id
+  }
+}
