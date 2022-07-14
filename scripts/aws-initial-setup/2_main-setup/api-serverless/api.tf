57358ae06dec561997d1bb62801b8f3dacbb3228
davwys <david.wyss@hotmail.ch>
2022-07-14 15:35:46 +0200

    Lambda stuff

--- scripts/aws-initial-setup/2_main-setup/api-serverless/api.tf
+++ scripts/aws-initial-setup/2_main-setup/api-serverless/api.tf
@@ -7,6 +7,7 @@ resource "aws_lambda_function" "api_lambda" {
   filename      = var.api_source_code_object_id
   function_name = "${var.project}-${var.type}-api-lambda"
   role          = aws_iam_role.lambda_iam.arn
+  # Lambda handler function location; since this is in src/lambda.ts -> handler(), we use AWS' lambda syntax below
   handler       = "src/lambda.handler"
   source_code_hash = var.api_source_code_object_hash
 
