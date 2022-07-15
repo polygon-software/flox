7f5ed504be12ddda15dfba3ea9c673347e427b68
davwys <david.wyss@hotmail.ch>
2022-07-15 08:34:24 +0200

    Add iam

--- scripts/aws-initial-setup/2_main-setup/api-serverless/iam.tf
+++ scripts/aws-initial-setup/2_main-setup/api-serverless/iam.tf
@@ -1,19 +1,20 @@
 resource "aws_iam_role" "lambda_iam" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+  name = "${var.project}-${var.type}-lambda-iam"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
     }
-  ]
+    ]
+  })
 }
-EOF
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_iam.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
