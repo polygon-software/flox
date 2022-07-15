25cfb10bffd0ede83c99dd565d0b653c625350d9
davwys <david.wyss@hotmail.ch>
2022-07-15 08:56:22 +0200

    TF cleanup, path fixes

--- scripts/aws-initial-setup/2_main-setup/api-serverless/api.tf
+++ scripts/aws-initial-setup/2_main-setup/api-serverless/api.tf
@@ -37,8 +37,9 @@ resource "aws_lambda_function" "api_lambda" {
       COMPOSE_PROJECT_NAME = var.project,
       USER_POOL_ID= var.user_pool_id,
       USER_POOL_CLIENT_ID = var.user_pool_client_id,
-      BASE_URL = "https://${var.domain}""DEV" = "false"
-      CLOUDWATCH_GROUP_NAME = "${var.project}-${var.type}"
+      BASE_URL = "https://${var.domain}",
+      "DEV" = "false",
+      CLOUDWATCH_GROUP_NAME = "${var.project}-${var.type}",
       CLOUDWATCH_STREAM_NAME = "API", // TODO with project & type?
       AWS_LOG_BUCKET_NAME = var.log_bucket_id
     }
