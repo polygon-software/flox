e954766355300e3708f688a1d11188eec45d4b59
davwys <david.wyss@hotmail.ch>
2022-07-19 14:45:52 +0200

    Initial CORS setup for API gateway

--- scripts/aws-initial-setup/2_main-setup/api-serverless/outputs.tf
+++ scripts/aws-initial-setup/2_main-setup/api-serverless/outputs.tf
@@ -1,6 +1,4 @@
 output "api_endpoint" {
-  description = "Public function URL of the Lambda function"
-  value       =   aws_api_gateway_deployment.api_gateway_deployment.invoke_url // TODO make route53 record...
-  #  value       = aws_api_gateway_domain_name.api_domain.domain_name
-#  value       = aws_lambda_function_url.api_lambda_url.function_url
+  description = "Public access URL of the API"
+  value       = aws_api_gateway_domain_name.api_domain.domain_name
 }
