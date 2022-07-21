b4d4710725569de11a6f916d5164ee9bd2f5abfd
davwys <david.wyss@hotmail.ch>
2022-07-21 10:21:08 +0200

    Docs

--- scripts/aws-initial-setup/2_main-setup/api-serverless/cors.tf
+++ scripts/aws-initial-setup/2_main-setup/api-serverless/cors.tf
@@ -60,6 +60,7 @@ resource "aws_api_gateway_integration_response" "options" {
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'" // TODO: only from var.domain
+    // Access only allowed from frontend domain
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.domain}'"
   }
 }
