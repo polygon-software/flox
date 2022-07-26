25e418e72e481ed66ed7936e98dd08ceb37e430e
davwys <david.wyss@hotmail.ch>
2022-07-26 17:12:33 +0200

    JWT testing setup

--- backend/src/flox/modules/auth/jwt.strategy.ts
+++ backend/src/flox/modules/auth/jwt.strategy.ts
@@ -21,7 +21,7 @@ export class JwtStrategyValidationResult {
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'), // TODO ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKeyProvider: passportJwtSecret({
         cache: true,
