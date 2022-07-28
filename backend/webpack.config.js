f1554353aab7d861cfcd61ac949143a87a250796
davwys <david.wyss@hotmail.ch>
2022-07-28 16:32:27 +0200

    Basic webpack config

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,54 +1,67 @@
-// // Webpack configuration (used for Lambda / serverless deployment)
-// module.exports = (options, webpack) => {
-//   const lazyImports = [
-//     '@nestjs/microservices/microservices-module',
-//     '@nestjs/websockets/socket-module',
-//     '@nestjs/sequelize',
-//     '@nestjs/terminus',
-//     '@nestjs/mongoose',
-//     'apollo-server-fastify',
-//     '@apollo/subgraph',
-//     '@apollo/gateway',
-//     'class-transformer/storage',
-//     'pg-native',
-//     '@mikro-orm/core',
-//   ];
-//
-//   return {
-//     module: {
-//       rules: [
-//         {
-//           test: /\.ts$/,
-//           use: {
-//             loader: 'babel-loader',
-//             options: {
-//               presets: ['@babel/preset-env', '@babel/preset-typescript'],
-//             },
-//           },
-//           exclude: /node_modules/,
-//         },
-//       ],
-//     },
-//     resolve: {
-//       extensions: ['.ts'],
-//     },
-//     ...options,
-//     externals: [],
-//     plugins: [
-//       ...options.plugins,
-//       new webpack.CleanPlugin(),
-//       new webpack.IgnorePlugin({
-//         checkResource(resource) {
-//           if (lazyImports.includes(resource)) {
-//             try {
-//               require.resolve(resource);
-//             } catch (err) {
-//               return true;
-//             }
-//           }
-//           return false;
-//         },
-//       }),
-//     ],
-//   };
-// };
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const isProduction =
+  typeof process.env.NODE_ENV !== 'undefined' &&
+  process.env.NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
+const devtool = isProduction ? false : 'inline-source-map';
+
+const lazyImports = [
+  '@nestjs/microservices/microservices-module',
+  '@nestjs/websockets/socket-module',
+  'ts-loader',
+  'aws-lambda'
+];
+
+module.exports = {
+  entry: './src/main.ts',
+  optimization: {
+    minimize: false,
+  },
+  target: 'node',
+  mode,
+  devtool,
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
+    new ForkTsCheckerWebpackPlugin({
+      tslint: true,
+    }),
+    new webpack.DefinePlugin({
+      CONFIG: JSON.stringify(require('config')),
+    }),
+    new webpack.IgnorePlugin({
+      checkResource(resource) {
+        if (lazyImports.includes(resource)) {
+          try {
+            require.resolve(resource);
+          } catch (err) {
+            return true;
+          }
+        }
+        return false;
+      },
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+};
