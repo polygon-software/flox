05012381bf1df60824bd4e6fd8bfe06be2a2a1a1
davwys <david.wyss@hotmail.ch>
2022-07-29 14:04:14 +0200

    Set up per-mode configuration of infrastructure

--- scripts/support/create-flox-tfvars.sh
+++ scripts/support/create-flox-tfvars.sh
@@ -1,9 +1,10 @@
 # ========================
 # Creates flox.tfvars file (Terraform environment variables) from
 # .env and flox.config.json where relevant to Terraform variables
+# Takes one parameter: 'live', 'test' or 'dev'
 # ========================
 
-echo "Creating flox.tfvars from configurations..."
+echo "Creating flox.tfvars from configurations for $1 mode..."
 
 # Remove existing file (if any)
 rm -f flox.tfvars
@@ -11,8 +12,7 @@ rm -f flox.tfvars
 # ===== Flox config variables =====
 
 # Apply flox.config.json via helper .ts file
-ts-node --compiler-options '{"module": "commonjs"}' ./export-variables.ts  >> flox.tfvars
-
+ts-node --compiler-options '{"module": "commonjs"}' ./export-variables.ts "$1"  >> flox.tfvars
 
 echo "flox.tfvars created successfully!"
 
