3e7c20876eaf1a31ea6bd9324f432868b7a7b939
davwys <david.wyss@hotmail.ch>
2022-08-10 11:17:16 +0200

    Fix security groups

--- scripts/aws-initial-setup/2_main-setup/api-ebs/dns.tf
+++ scripts/aws-initial-setup/2_main-setup/api-ebs/dns.tf
@@ -5,23 +5,3 @@ resource "aws_route53_record" "api_record" {
   ttl                   = "300"
   records               = [aws_elastic_beanstalk_environment.api_env.endpoint_url]
 }
-
-resource "aws_security_group" "api_security_group" {
-  name                  = "${var.project}-${var.type}-api-security-group"
-  vpc_id                = var.vpc_id
-
-  ingress {
-    from_port         = 3000
-    protocol          = "TCP"
-    to_port           = 3000
-    cidr_blocks       = ["0.0.0.0/0"]
-    ipv6_cidr_blocks  = ["::/0"]
-  }
-  egress {
-    from_port         = 0
-    to_port           = 0
-    protocol          = "-1"
-    cidr_blocks       = ["0.0.0.0/0"]
-    ipv6_cidr_blocks  = ["::/0"]
-  }
-}
