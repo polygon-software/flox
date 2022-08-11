9bf59f8df10d74fe9ae42ca8306d786a4e37c0ac
davwys <david.wyss@hotmail.ch>
2022-08-11 13:14:04 +0200

    Test update

--- frontend/src/components/menu/MenuBar.vue
+++ frontend/src/components/menu/MenuBar.vue
@@ -8,7 +8,7 @@
           class="q-ma-sm"
       >
       <h5 class="text-black q-pa-none q-ma-md">
-        Flox YEET HAS BEEN UPDATED
+        Flox Template
       </h5>
     </div>
   <div class="row">
