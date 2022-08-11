1d2e2e9933ee128e4c5a654c577a4d0245f96bf4
davwys <david.wyss@hotmail.ch>
2022-08-11 15:36:38 +0200

    use actual cert

--- scripts/aws-initial-setup/2_main-setup/web-ssr/web.tf
+++ scripts/aws-initial-setup/2_main-setup/web-ssr/web.tf
@@ -30,11 +30,6 @@ resource "aws_elastic_beanstalk_application_version" "frontend_application_versi
   }
 }
 
-// Link to data object instead of actual resource in order not to break update flow (which may attempt recreation)
-data "aws_acm_certificate" "frontend_cert" {
-  domain = var.domain
-}
-
 // Create EBS environment
 // For settings, see https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
 resource "aws_elastic_beanstalk_environment" "frontend_env" {
@@ -105,7 +100,7 @@ resource "aws_elastic_beanstalk_environment" "frontend_env" {
   setting {
     namespace     = "aws:elbv2:listener:443"
     name          = "SSLCertificateArns"
-    value         = data.aws_acm_certificate.frontend_cert.arn
+    value         = aws_acm_certificate.frontend_cert.arn
   }
 
   setting {
