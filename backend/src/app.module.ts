e46d9e9899a348206b5f9f27fd107baecb96cdd2
davwys <david.wyss@hotmail.ch>
2022-08-16 08:23:38 +0200

    Remove required keys, add TODO

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -41,10 +41,11 @@ import { isServerless } from './flox/core/flox-helpers';
         // AWS
         AWS_MAIN_REGION: Joi.string().required(),
         AWS_REGION: Joi.string().required(),
-        AWS_S3_ACCESS_KEY_ID: Joi.string().required(),
-        AWS_S3_SECRET_ACCESS_KEY: Joi.string().required(),
-        AWS_SES_ACCESS_KEY_ID: Joi.string().required(),
-        AWS_SES_SECRET_ACCESS_KEY: Joi.string().required(),
+        // TODO: handle in separate PR, how do we want to handle keys?
+        // AWS_S3_ACCESS_KEY_ID: Joi.string().required(),
+        // AWS_S3_SECRET_ACCESS_KEY: Joi.string().required(),
+        // AWS_SES_ACCESS_KEY_ID: Joi.string().required(),
+        // AWS_SES_SECRET_ACCESS_KEY: Joi.string().required(),
         AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
         AWS_PRIVATE_BUCKET_NAME: Joi.string().required(),
       }),
