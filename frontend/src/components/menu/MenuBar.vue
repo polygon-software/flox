ea54ffb289aae3310468ea853b649d7c90a30904
Marinolino <45514278+Marinolino@users.noreply.github.com>
2022-08-25 13:22:24 +0200

    fix: Test fix (#403)
    * test fix
    
    * semantic bs

--- frontend/src/components/menu/MenuBar.vue
+++ frontend/src/components/menu/MenuBar.vue
@@ -8,7 +8,7 @@
           class="q-ma-sm"
       >
       <h5 class="text-black q-pa-none q-ma-md">
-        Flox Template Banano
+        Flox Template Test
       </h5>
     </div>
   <div class="row">
