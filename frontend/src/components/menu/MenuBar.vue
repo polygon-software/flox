a2751646e62b046d3dc7118a65ff67c3abfa913e
Marinolino <45514278+Marinolino@users.noreply.github.com>
2022-08-25 12:49:53 +0200

    feat: empty S3 bucket before destroy (#401)
    * empty S3 bucket before destroy
    
    * SeManTiC PR
    
    * fixed aÃpply line

--- frontend/src/components/menu/MenuBar.vue
+++ frontend/src/components/menu/MenuBar.vue
@@ -8,7 +8,7 @@
           class="q-ma-sm"
       >
       <h5 class="text-black q-pa-none q-ma-md">
-        Flox Template
+        Flox Template Banano
       </h5>
     </div>
   <div class="row">
