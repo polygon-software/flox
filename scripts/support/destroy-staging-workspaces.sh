b62c2a6ef4fadcaeef94ffd918240d60edb90ca2
davwys <david.wyss@hotmail.ch>
2022-08-26 10:10:23 +0200

    Working destroy script state

--- scripts/support/destroy-staging-workspaces.sh
+++ scripts/support/destroy-staging-workspaces.sh
@@ -1,19 +1,45 @@
+# ==========================================================================
+# Destroys all staging Terraform cloud workspaces for a given staging system
+# Takes one parameter: the staging branch name (e.g. 'stage-250813')
+# ==========================================================================
+# TODO: ensure 'stage' tag on creation...
+
+stage_branch=$1
+
+REGEX_STAGE="^stage-[0-9]{6}$"
+if [[ ! $stage_branch =~ $REGEX_STAGE ]]
+then
+  echo "Invalid staging branch '$stage_branch'"
+  exit 1
+fi
+
 cd destroy-staging-workspaces || exit 1
 
-# Initialize
+# Initialize & select first workspace (since default may not exist)
 terraform init | echo 1
 
+# Create temp workspace & switch to it, so we can delete all staging workspaces
 terraform workspace new temp
 terraform workspace select temp
 workspaces=$(terraform workspace list)
 
+destroyed_workspaces=0
 
 while IFS= read -r workspace;
 do
-  if [[ $workspace == *"-stage-"* ]]; then
-    echo "$workspace is staging workspace!"
+  # Trim whitespace
+  workspace=$(echo "$workspace" | xargs)
+
+  # Find corresponding staging workspaces
+  if [[ $workspace == *"-$stage_branch"* ]]; then
+    echo "Deleting workspace '$workspace'..."
+
+    # Destroy workspace
+    terraform workspace delete -force "$workspace"
+
+    # Increment counter
+    ((destroyed_workspaces+=1))
   fi
 done <<< "$workspaces"
 
-
-terraform workspace delete temp
+echo "Done! Deleted $destroyed_workspaces cloud workspaces."
