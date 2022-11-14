eb8ecd4e31fee7c4ad5ba27bff9c7eb6e2e20075
davwys <david.wyss@hotmail.ch>
2022-11-14 18:05:32 +0100

    Fix i18n bogo

--- frontend/src/i18n/en/index.ts
+++ frontend/src/i18n/en/index.ts
@@ -191,7 +191,7 @@ export default {
     admin_panel: 'Admin Panel',
     system_notification: 'System Notifications',
     broadcast_message:
-      'Notify your system's users about updates, downtime and new features. Warning: these messages will be immediately visible for all users in your system and should hence only be sent with care.',
+      "Notify your system's users about updates, downtime and new features. Warning: these messages will be immediately visible for all users in your system and should hence only be sent with care.",
   },
   access_control: {
     users: 'Users',
