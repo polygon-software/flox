d7e3440cc04816f6e2cdbfb21ed791c8dd1fbcb1
Marinolino <45514278+Marinolino@users.noreply.github.com>
2023-02-06 14:46:47 +0100

    Update frontend/src/flox/modules/form/data/REGEX.ts
    Co-authored-by: David Wyss <david.wyss@hotmail.ch>

--- frontend/src/flox/modules/form/data/REGEX.ts
+++ frontend/src/flox/modules/form/data/REGEX.ts
@@ -6,6 +6,5 @@ export const PHONE_NUMBER_REGEX = /^((\+41\d{9})|(\+423\d{7}))$/;
 export const IBAN_REGEX = /^[A-Z]{2}\d{2}( [A-Z0-9]{4}){4} [A-Z0-9]$/;
 export const MONTH_DATE_REGEX = /^(0(1-9)|10|11|12)\/(19|20)\d{2}$/;
 export const TIME_REGEX = /^([0-1]?\d|2[0-3]):[0-5]\d$/;
-export const FORMULA_REGEX = /^([\d.]+|[+*\-/]+|[()]+|[ALPRF]|\s+)+$/;
 export const URL_REGEX =
   /^[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
