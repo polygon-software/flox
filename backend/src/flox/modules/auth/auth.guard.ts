d24c0ec6a3ad39278921edd6fd15b56e4878afd5
Johann Schwabe <johann.schwabe@uzh.ch>
2023-02-14 10:30:06 +0100

    fix: public endpoints not accessible

--- backend/src/flox/modules/auth/auth.guard.ts
+++ backend/src/flox/modules/auth/auth.guard.ts
@@ -1,13 +1,26 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
 
 import { getRequest } from '../../core/flox-helpers';
+
+import { IS_PUBLIC_KEY } from './authentication.decorator';
 
 /**
  * JSON Web token authentication guard
  */
 @Injectable()
 export default class JwtAuthGuard extends AuthGuard('jwt') {
+  /**
+   * Constructor
+   *
+   * @param reflector - reflector
+   */
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
   /**
    * Gets the request from context
    *
@@ -16,5 +29,25 @@ export default class JwtAuthGuard extends AuthGuard('jwt') {
    */
   getRequest(context: ExecutionContext): ReturnType<typeof getRequest> {
     return getRequest(context);
+  }
+
+  /**
+   * Determines whether a user can activate the route based on the authentication status
+   *
+   * @param  context - execution context
+   * @returns - whether the user can activate
+   */
+  public canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    // For publicly accessible resources, allow access by default
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
   }
 }
