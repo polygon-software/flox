5d0b9b19a2691b942473a5bebea24660a389b73f
thommann <thomasrolf.mannhart@uzh.ch>
2023-04-03 14:48:38 +0200

    general create user input to inherit

--- backend/src/flox/modules/auth/dto/input/signup-create-user.input.ts
+++ backend/src/flox/modules/auth/dto/input/signup-create-user.input.ts
@@ -1,42 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
-import {
-  IsEmail,
-  IsLocale,
-  IsLowercase,
-  IsOptional,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
-import CreateInput from '../../../abstracts/crud/dto/input/create.input';
 import { PASSWORD_REGEX } from '../../../../REGEX';
 
-@InputType()
-export default class SignupCreateUserInput extends CreateInput {
-  @Field(() => String)
-  @IsString()
-  @MinLength(6)
-  @MaxLength(50)
-  username: string;
+import CreateUserInput from './create-user.input';
 
+@InputType()
+export default class SignupCreateUserInput extends CreateUserInput {
   @Field(() => String, { description: 'password' })
   @IsString()
   @Matches(PASSWORD_REGEX)
   password: string;
-
-  @Field(() => String)
-  @IsString()
-  @IsEmail()
-  email: string;
-
-  @Field(() => String, { nullable: true })
-  @IsString()
-  @MinLength(2)
-  @MaxLength(2)
-  @IsLowercase()
-  @IsLocale()
-  @IsOptional()
-  lang: string;
 }
