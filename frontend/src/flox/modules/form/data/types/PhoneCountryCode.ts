253658d2526c4c759e31b19d1108afbff3699260
thommann <thomasrolf.mannhart@uzh.ch>
2023-04-03 17:46:59 +0200

    rm todo

--- frontend/src/flox/modules/form/data/types/PhoneCountryCode.ts
+++ frontend/src/flox/modules/form/data/types/PhoneCountryCode.ts
@@ -4,7 +4,6 @@ import { CountryCode } from 'libphonenumber-js';
  * This type represents a country's phone number code (e.g. +41 for CH) and the input mask
  * for a valid number (e.g. ## ### ## ## for CH), if there is a fixed one
  */
-// TODO: Maybe create class country code instead of using package?
 export type PhoneCountryCode = {
   code: CountryCode;
   label: string;
